c5928b3ac0e0ac8e142f2ce308019367699657aa
Paul Klimashkin <klimashkin@gmail.com>
2012-04-18 18:45:45 +0400

    Настройка Enviroment Использование LESS

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-var express = require('express'),
-    app = express.createServer();
+#!/usr/bin/env node
 
+var express = require('express'),
+	lessMiddleware = require('less-middleware'),
+    app = express.createServer(),
+	oneDay = 86400000,
+	oneYear = 31557600000;
+
+/**
+ * Окружение (development, test, production)
+ */
+var env = process.env.NODE_ENV || 'development';
+	
 app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.set('view options', {layout: false});
     app.use(express.methodOverride());
     app.use(express.bodyParser());
+	
+	if (env=='development') {
+		app.use('/style', lessMiddleware({src: __dirname + '/style', force: true, once: false, compress: false, debug:true}));
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+		require('reloader')({
+			watchModules: true,
+			onStart: function () {console.log('Started on port: 3000');},
+			onReload: function () {app.listen(3000);}
+		});
+	} else {
+		app.use('/style', lessMiddleware({src: __dirname + '/style', force: false, once: true, compress: true, optimization:2, debug:false}));
+		app.use(express.errorHandler());
+	}
+	
     app.use(app.router);
+	app.use('/js', express.static(__dirname + '/js', {maxAge: oneDay, redirect: '/'}));
+	app.use('/style', express.static(__dirname + '/style'));
+	
 });
 
-app.configure('development', function(){
-    var oneDay = 86400000;
-    app.use('/js', express.static(__dirname + '/js/', { maxAge: oneDay, redirect: '/css'}));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-    var oneYear = 31557600000;
-    app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-    app.use(express.errorHandler());
-});
 
 app.get('/', function(req, res){
-    res.send('hello world');
+	res.render('index.jade', { pageTitle: 'My Site', youAreUsingJade: true });
 });
 
-app.listen(3000);
+if (env!='development') app.listen(3000);
