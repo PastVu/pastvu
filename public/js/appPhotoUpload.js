1d57b0784370f9e8070a701dd814b1aa906ecbfb
Paul Klimashkin <klimashkin@gmail.com>
2012-07-24 14:56:10 +0400

    Upload preview работает в первом приближении

--- public/js/appPhotoUpload.js
+++ public/js/appPhotoUpload.js
@@ -16,7 +16,10 @@ requirejs.config({
 		'Utils': 'lib/Utils',
 		'Browser': 'lib/Browser',
 		'jquery.ui.widget': 'lib/jquery/ui/jquery.ui.widget',
-		'jquery.fileupload': 'lib/jquery/plugins/fileupload'
+		'jquery.fileupload': 'lib/jquery/plugins/fileupload',
+		'load-image': 'lib/jquery/plugins/fileupload/load-image',
+		'tmpl': 'lib/jquery/plugins/fileupload/tmpl',
+		'canvas-to-blob': 'lib/jquery/plugins/fileupload/canvas-to-blob'
 	}
 });
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -31,12 +34,14 @@ require([
 	'knockout', 'knockout.mapping',
 	'mvvm/GlobalParams', 'mvvm/User', 'mvvm/TopPanel', 'mvvm/i18n',
 	'KeyHandler', 'auth',
-	'jquery.ui.widget', 'jquery.fileupload/jquery.iframe-transport', 'jquery.fileupload/jquery.fileupload'
+	'jquery.ui.widget', 
+	'jquery.fileupload/jquery.iframe-transport', 'jquery.fileupload/jquery.fileupload', 'jquery.fileupload/jquery.fileupload-ui', 'jquery.fileupload/locale'
 ],function(domReady, $, Browser, Utils, socket, ET, ko, ko_mapping, GlobalParams, User, TopPanel, i18n, keyTarget, auth) {
+	'use strict';
 	console.timeStamp('Require app Ready');
 	var login, reg, recall,
 		profileView, profileVM,
-		uploadVM;
+		uploadVM, fileupload;
 	
 	$.when(LoadParams(), waitForDomReady())
 	 .pipe(auth.LoadMe)
@@ -72,12 +77,61 @@ require([
 			},
 			owner: uploadVM
 		}),
+		
+		toUpload: ko.observableArray([])
 	};
 		
 	function app () {
 		new TopPanel('top_panel_fringe');
 		ko.applyBindings(uploadVM, document.getElementById('now'));
 		
+		fileupload = $('#fileupload');
+		// Initialize the jQuery File Upload widget:
+		fileupload.fileupload();
+		
+        // Load existing files:
+        $('#fileupload').each(function () {
+            var that = this;
+            $.getJSON(this.action, function (result) {
+                if (result && result.length) {
+                    $(that).fileupload('option', 'done')
+                        .call(that, null, {result: result});
+                }
+            });
+        });
+		
+		$('#fileupload').fileupload('option', {
+            url: '//jquery-file-upload.appspot.com/',
+            maxFileSize: 5000000,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+            process: [
+                {
+                    action: 'load',
+                    fileTypes: /^image\/(gif|jpeg|png)$/,
+                    maxFileSize: 20000000 // 20MB
+                },
+                {
+                    action: 'resize',
+                    maxWidth: 1440,
+                    maxHeight: 900
+                },
+                {
+                    action: 'save'
+                }
+            ]
+        });
+        // Upload server status check for browsers with CORS support:
+        if ($.support.cors) {
+            $.ajax({
+                url: '//jquery-file-upload.appspot.com/',
+                type: 'HEAD'
+            }).fail(function () {
+                $('<span class="alert alert-error"/>')
+                    .text('Upload server currently unavailable - ' +
+                            new Date())
+                    .appendTo('#fileupload');
+            });
+        }
 	}
 	
 });
