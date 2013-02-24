ed8da9d1409aa543bd4200de9a9ca6ec75734cf1
klimashkin <klimashkin@gmail.com>
2013-02-24 20:54:18 +0400

    Составной индекс для кластеров (geo+zoom) увеливает производительность выборки на 30%

--- models/Cluster.js
+++ models/Cluster.js
@@ -1,45 +1,48 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    _ = require('lodash');
+	Schema = mongoose.Schema,
+	_ = require('lodash');
 
 var ClusterSheme = new mongoose.Schema(
-        {
-            geo: {type: [Number], index: '2d'}, // Координаты левого верхнего угла
-            gravity: {type: [Number]}, // Координаты центра тяжести кластера
-            //rel: {type: [Number]}, // Координаты левого верхнего угла (множитель отнсительно 0,0) для справки
+		{
+			geo: {type: [Number]}, // Координаты левого верхнего угла
+			z: {type: Number}, // Зум кластера
 
-            z: {type: Number, index: true}, // Зум кластера
-            c: {type: Number}, // Количество фотографий в кластере
-            file: {type: String}, // Обложка кластера
-            p: [
-                { type: Schema.Types.ObjectId, ref: 'Photo' } // Массив id фотографий в кластере
-            ]
-        },
-        {
-            strict: true
-        }
-    ),
-    ClusterParamsSheme = new mongoose.Schema(
-        {
-            z: {type: Number, index: { unique: true }}, // Зум кластера
-            w: {type: Number}, // Ширина кластера в градусах
-            h: {type: Number}, // Высота кластера в градусах
+			gravity: {type: [Number]}, // Координаты центра тяжести кластера
+			//rel: {type: [Number]}, // Координаты левого верхнего угла (множитель отнсительно 0,0) для справки
 
-            // Эти поля общие для всех параметров, так что такой документ в коллекции будет один
-            sgeo: {type: [Number]}, // [lng, lat] центра расчетного кластера
-            sz: {type: Number}, // Зум, с которого считали кластеры
-            sw: {type: Number}, // Ширина кластера в пикселях
-            sh: {type: Number} // Высота кластера в пикселях
-        },
-        {
-            strict: true
-        }
-    );
+			c: {type: Number}, // Количество фотографий в кластере
+			file: {type: String}, // Обложка кластера
+			p: [
+				{ type: Schema.Types.ObjectId, ref: 'Photo' } // Массив id фотографий в кластере
+			]
+		},
+		{
+			strict: true
+		}
+	),
+	ClusterParamsSheme = new mongoose.Schema(
+		{
+			z: {type: Number, index: { unique: true }}, // Зум кластера
+			w: {type: Number}, // Ширина кластера в градусах
+			h: {type: Number}, // Высота кластера в градусах
+
+			// Эти поля общие для всех параметров, так что такой документ в коллекции будет один
+			sgeo: {type: [Number]}, // [lng, lat] центра расчетного кластера
+			sz: {type: Number}, // Зум, с которого считали кластеры
+			sw: {type: Number}, // Ширина кластера в пикселях
+			sh: {type: Number} // Высота кластера в пикселях
+		},
+		{
+			strict: true
+		}
+	);
+
+ClusterSheme.index({ geo: '2d', z: 1 }); // Compound index   http://docs.mongodb.org/manual/core/geospatial-indexes/#compound-geospatial-indexes
 
 
 module.exports.makeModel = function (db) {
-    db.model('Cluster', ClusterSheme);
-    db.model('ClusterParams', ClusterParamsSheme);
+	db.model('Cluster', ClusterSheme);
+	db.model('ClusterParams', ClusterParamsSheme);
 };
