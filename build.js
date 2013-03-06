6c925c0bebe0cbb522c82d813aa3d0a8f9b74380
klimashkin <klimashkin@gmail.com>
2013-03-06 15:31:45 +0400

    Проба собрать подмодуль включив в него только jade и css

--- build.js
+++ build.js
@@ -1,275 +1,282 @@
 #!/usr/bin/env node
 var start = Date.now(),
 	fs = require('fs'),
-    path = require('path'),
-    sys = require('util'),
-    step = require('step'),
-    File = require('file-utils').File,
-    requirejs = require('requirejs'),
-    less = require('less'),
-    jade = require('jade'),
-    _ = require('lodash'),
-    Utils = require('./commons/Utils.js'),
-    version = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8')).version,
+	path = require('path'),
+	sys = require('util'),
+	step = require('step'),
+	File = require('file-utils').File,
+	requirejs = require('requirejs'),
+	less = require('less'),
+	jade = require('jade'),
+	_ = require('lodash'),
+	Utils = require('./commons/Utils.js'),
+	version = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8')).version,
 
-    jadeCompileOptions = {
-        pretty: false,
-        pageTitle: 'OldMos51',
-        appHash: Utils.randomString(10),
-        appVersion: version
-    },
+	jadeCompileOptions = {
+		pretty: false,
+		pageTitle: 'OldMos51',
+		appHash: Utils.randomString(10),
+		appVersion: version
+	},
 
-    lessCompileOptions = {
-        compress: true,
-        yuicompress: true,
-        optimization: 2,
-        silent: false,
-        path: 'public/style/',
-        color: true,
-        strictImports: true
-    },
+	lessCompileOptions = {
+		compress: true,
+		yuicompress: true,
+		optimization: 2,
+		silent: false,
+		path: 'public/style/',
+		color: true,
+		strictImports: true
+	},
 
-    requireBuildConfig = {
-        appDir: "public/",
-        baseUrl: 'js',
-        dir: "public-build",
-        keepBuildDir: false,
-        optimize: "uglify",
-        uglify: {
-            toplevel: false,
-            ascii_only: false,
-            beautify: false,
-            no_mangle: false
-        },
-        skipDirOptimize: false, //Оптимизировать только модули (modules array), не трогая остальные js
-        optimizeCss: "none", //Не трогаем css
-        preserveLicenseComments: false, //Удаляем лицензионные комментарии
-        removeCombined: false, //Не удаляем файлы, которые заинлайнились в модуль
-        inlineText: true, //Включать ли в модули контент, загруженный плагином text
-        logLevel: 0,
-        mainConfigFile: 'public/js/_mainConfig.js',
-        modules: [
-            {
-                name: "_mainConfig" //Компилируем конфигурацию, чтобы включить туда общую зависимость 'lib/JSExtensions'
-            },
-            {
-                name: "appMap",
-                include: ['m/auth', 'm/top', 'm/map/map']
-            },
-            {
-                name: "appUser",
-                include: ['m/auth', 'm/top', 'm/user/brief', 'm/user/menu']
-            },
-            {
-                name: "appPhoto",
-                include: ['m/auth', 'm/top', 'm/photo/photo']
-            }
-        ]
-    },
-    jadeFiles = [],
-    lessFiles = [];
+	requireBuildConfig = {
+		appDir: "public/",
+		baseUrl: 'js',
+		dir: "public-build",
+		keepBuildDir: false,
+		optimize: "uglify",
+		uglify: {
+			toplevel: false,
+			ascii_only: false,
+			beautify: false,
+			no_mangle: false
+		},
+		skipDirOptimize: false, //Оптимизировать только модули (modules array), не трогая остальные js
+		optimizeCss: "none", //Не трогаем css
+		preserveLicenseComments: false, //Удаляем лицензионные комментарии
+		removeCombined: false, //Не удаляем файлы, которые заинлайнились в модуль
+		inlineText: true, //Включать ли в модули контент, загруженный плагином text
+		logLevel: 0,
+		mainConfigFile: 'public/js/_mainConfig.js',
+		modules: [
+			{
+				name: "_mainConfig" //Компилируем конфигурацию, чтобы включить туда общую зависимость 'lib/JSExtensions'
+			},
+			{
+				name: "appMap",
+				include: ['m/auth', 'm/top', 'm/map/map']
+			},
+			{
+				name: "appUser",
+				include: ['m/auth', 'm/top', 'm/user/brief', 'm/user/menu']
+			},
+			{
+				name: "m/user/gallery",
+				exclude: [
+					'underscore', 'Browser', 'Utils', 'socket', 'Params', 'knockout', 'knockout.mapping', 'm/_moduleCliche', 'globalVM', 'renderer', 'm/Photo', 'm/storage',
+					'text', 'css'
+				]
+			},
+			{
+				name: "appPhoto",
+				include: ['m/auth', 'm/top', 'm/photo/photo']
+			}
+		]
+	},
+	jadeFiles = [],
+	lessFiles = [];
 
 step(
-    //Находим клиентские jade-шаблоны и создаем плоский массив и создаем временную папку tpl для рендеренных
-    function searchJades() {
-        var tplFolder = new File('./views/client'),
-            tplFolderTemp = new File('./' + requireBuildConfig.appDir + 'tpl'),
-            _this = this;
+	//Находим клиентские jade-шаблоны и создаем плоский массив и создаем временную папку tpl для рендеренных
+	function searchJades() {
+		var tplFolder = new File('./views/client'),
+			tplFolderTemp = new File('./' + requireBuildConfig.appDir + 'tpl'),
+			_this = this;
 
-        tplFolder.list(function (e, files) {
-            if (e) {
-                console.dir(e);
-                process.exit(1);
-            }
-            jadeFiles = Utils.filesRecursive(files, '');
+		tplFolder.list(function (e, files) {
+			if (e) {
+				console.dir(e);
+				process.exit(1);
+			}
+			jadeFiles = Utils.filesRecursive(files, '');
 
-            //Создаём временные директории и поддиректории для скомпилированных Jade-шаблонов
-            tplFolderTemp.createDirectory();
-            tplFolderTemp.removeOnExit(); //Удаляем временную папку скомпилированных шаблонов после завершения сборки
-            Object.keys(files).forEach(function (element, index, array) {
-                if (Utils.isType('object', files[element])) {
-                    new File('./' + requireBuildConfig.appDir + 'tpl/' + element).createDirectory(_this.parallel());
-                }
-            });
-            _this.parallel()();
-        });
-    },
+			//Создаём временные директории и поддиректории для скомпилированных Jade-шаблонов
+			tplFolderTemp.createDirectory();
+			tplFolderTemp.removeOnExit(); //Удаляем временную папку скомпилированных шаблонов после завершения сборки
+			Object.keys(files).forEach(function (element, index, array) {
+				if (Utils.isType('object', files[element])) {
+					new File('./' + requireBuildConfig.appDir + 'tpl/' + element).createDirectory(_this.parallel());
+				}
+			});
+			_this.parallel()();
+		});
+	},
 
-    //Ищем less-файлы для компиляции и создаем плоский массив
-    function searchLess() {
-        var lessFolder = new File('./' + requireBuildConfig.appDir + 'style'),
-            _this = this;
+	//Ищем less-файлы для компиляции и создаем плоский массив
+	function searchLess() {
+		var lessFolder = new File('./' + requireBuildConfig.appDir + 'style'),
+			_this = this;
 
-        lessFolder.list(function (e, files) {
-            if (e) {
-                console.dir(e);
-                process.exit(1);
-            }
-            lessFiles = Utils.filesRecursive(files, '', ['bootstrap', 'fonts'], function getOnlyLess(element) {
-                return element.indexOf('.less') > -1;
-            });
-            _this();
-        });
-    },
+		lessFolder.list(function (e, files) {
+			if (e) {
+				console.dir(e);
+				process.exit(1);
+			}
+			lessFiles = Utils.filesRecursive(files, '', ['bootstrap', 'fonts'], function getOnlyLess(element) {
+				return element.indexOf('.less') > -1;
+			});
+			_this();
+		});
+	},
 
-    //Компилируем less и jade
-    function startCompile() {
-        lessCompile(lessFiles, this.parallel());
-        jadeCompile(jadeFiles, 'views/client/', requireBuildConfig.appDir + 'tpl/', this.parallel());
-    },
+	//Компилируем less и jade
+	function startCompile() {
+		lessCompile(lessFiles, this.parallel());
+		jadeCompile(jadeFiles, 'views/client/', requireBuildConfig.appDir + 'tpl/', this.parallel());
+	},
 
-    //Собираем require
-    function requireBuild() {
-        console.log('~~~ Start r.js build ~~~');
-        var _this = this;
-        requirejs.optimize(requireBuildConfig, function (buildResponse) {
-            //buildResponse is just a text output of the modules
-            //included. Load the built file for the contents.
-            //Use requireBuildConfig.out to get the optimized file contents.
-            //var contents = fs.readFileSync(requireBuildConfig.out, 'utf8');
-            console.log('Require build finished');
-            _this();
-        });
-    },
+	//Собираем require
+	function requireBuild() {
+		console.log('~~~ Start r.js build ~~~');
+		var _this = this;
+		requirejs.optimize(requireBuildConfig, function (buildResponse) {
+			//buildResponse is just a text output of the modules
+			//included. Load the built file for the contents.
+			//Use requireBuildConfig.out to get the optimized file contents.
+			//var contents = fs.readFileSync(requireBuildConfig.out, 'utf8');
+			console.log('Require build finished');
+			_this();
+		});
+	},
 
-    //Удаляем less из собранной директории
-    function removeLessFromBuild() {
-        var styleFolder = new File(requireBuildConfig.dir + '/style'),
-            _this = this;
+	//Удаляем less из собранной директории
+	function removeLessFromBuild() {
+		var styleFolder = new File(requireBuildConfig.dir + '/style'),
+			_this = this;
 
-        console.log('Removing Less from build');
-        styleFolder.list(function (e, files) {
-            if (e) {
-                console.dir(e);
-                process.exit(1);
-            }
-            lessFiles = Utils.filesRecursive(files, requireBuildConfig.dir + '/style/', null, function getOnlyLess(element) {
-                return element.indexOf('.less') > -1;
-            });
-            lessFiles.forEach(function (item, index) {
-                (new File(item)).remove(_this.parallel());
-            });
-        });
-    },
+		console.log('Removing Less from build');
+		styleFolder.list(function (e, files) {
+			if (e) {
+				console.dir(e);
+				process.exit(1);
+			}
+			lessFiles = Utils.filesRecursive(files, requireBuildConfig.dir + '/style/', null, function getOnlyLess(element) {
+				return element.indexOf('.less') > -1;
+			});
+			lessFiles.forEach(function (item, index) {
+				(new File(item)).remove(_this.parallel());
+			});
+		});
+	},
 
-    //Компилируем основные jade в статичные html
-    function compileMainJades() {
-        "use strict";
-        var tplFolder = new File('./views'),
-            toCompile = [],
-            _this = this;
+	//Компилируем основные jade в статичные html
+	function compileMainJades() {
+		"use strict";
+		var tplFolder = new File('./views'),
+			toCompile = [],
+			_this = this;
 
-        tplFolder.list(1, function (e, files) {
-            if (e) {
-                console.dir(e);
-                process.exit(1);
-            }
-            _.forEach(files, function(val, key) {
-                if (!Utils.isType('object', val)) {
-                    toCompile.push(key);
-                }
-            });
-            if (toCompile.length > 0) {
-                jadeCompile(toCompile, 'views/', requireBuildConfig.dir + '', _this.parallel(), 'html');
-            }
-            _this.parallel()();
-        });
-    },
+		tplFolder.list(1, function (e, files) {
+			if (e) {
+				console.dir(e);
+				process.exit(1);
+			}
+			_.forEach(files, function (val, key) {
+				if (!Utils.isType('object', val)) {
+					toCompile.push(key);
+				}
+			});
+			if (toCompile.length > 0) {
+				jadeCompile(toCompile, 'views/', requireBuildConfig.dir + '', _this.parallel(), 'html');
+			}
+			_this.parallel()();
+		});
+	},
 
-    function finish(e) {
-        if (e) {
-            console.dir(e);
-            process.exit(1);
-        }
-        console.log('Build complete. Ok in ' + (Date.now() - start) / 1000 + 's');
-    }
+	function finish(e) {
+		if (e) {
+			console.dir(e);
+			process.exit(1);
+		}
+		console.log('Build complete. Ok in ' + (Date.now() - start) / 1000 + 's');
+	}
 );
 
 
 function jadeCompile(files, inFolder, outFolder, done, replaceJade) {
-    var name, input, output,
-        fd,
-        i = 0;
+	var name, input, output,
+		fd,
+		i = 0;
 
-    next();
+	next();
 
-    function next() {
-        name = files[i++];
-        if (!name) {
-            return done();
-        }
-        input = inFolder + name;
-        output = outFolder + name;
-        if (replaceJade) {
-            output = output.substr(0, output.lastIndexOf('.jade') + 1) + replaceJade;
-        }
-        fs.readFile(input, 'utf-8', render);
-    }
+	function next() {
+		name = files[i++];
+		if (!name) {
+			return done();
+		}
+		input = inFolder + name;
+		output = outFolder + name;
+		if (replaceJade) {
+			output = output.substr(0, output.lastIndexOf('.jade') + 1) + replaceJade;
+		}
+		fs.readFile(input, 'utf-8', render);
+	}
 
-    function render(e, data) {
-        if (e) {
-            sys.puts("jade readFile error: " + e.message);
-            process.exit(1);
-        }
-        console.dir('Compiling Jade ' + input);
-        var fn = jade.compile(data, _.assign(_.clone(jadeCompileOptions, false), {filename: input}));
-        fd = fs.openSync(output, "w");
-        fs.writeSync(fd, fn(jadeCompileOptions), 0, "utf8");
-        fs.closeSync(fd);
-        next();
-    }
+	function render(e, data) {
+		if (e) {
+			sys.puts("jade readFile error: " + e.message);
+			process.exit(1);
+		}
+		console.dir('Compiling Jade ' + input);
+		var fn = jade.compile(data, _.assign(_.clone(jadeCompileOptions, false), {filename: input}));
+		fd = fs.openSync(output, "w");
+		fs.writeSync(fd, fn(jadeCompileOptions), 0, "utf8");
+		fs.closeSync(fd);
+		next();
+	}
 }
 
 function lessCompile(files, done) {
-    var input, output,
-        css, fd,
-        i = 0;
+	var input, output,
+		css, fd,
+		i = 0;
 
-    next();
+	next();
 
-    function next() {
-        input = files[i++];
-        if (!input) {
-            return done();
-        }
-        output = lessCompileOptions.path + input.replace('.less', '.css');
-        fs.readFile(lessCompileOptions.path + input, 'utf-8', parseLessFile);
-    }
+	function next() {
+		input = files[i++];
+		if (!input) {
+			return done();
+		}
+		output = lessCompileOptions.path + input.replace('.less', '.css');
+		fs.readFile(lessCompileOptions.path + input, 'utf-8', parseLessFile);
+	}
 
-    function parseLessFile(e, data) {
-        if (e) {
-            sys.puts("Error to read less " + (lessCompileOptions.path + input) + " file: " + e.message);
-            process.exit(1);
-        }
-        console.dir('Compiling LESS ' + lessCompileOptions.path + input);
-        new (less.Parser)({
-            paths: [lessCompileOptions.path + path.dirname(input)],
-            optimization: lessCompileOptions.optimization,
-            filename: path.basename(input),
-            strictImports: lessCompileOptions.strictImports
-        }).parse(data, function (err, tree) {
-                if (err) {
-                    less.writeError(err, lessCompileOptions);
-                    process.exit(1);
-                } else {
-                    try {
-                        css = tree.toCSS({
-                            compress: lessCompileOptions.compress,
-                            yuicompress: lessCompileOptions.yuicompress
-                        });
-                        if (output) {
-                            fd = fs.openSync(output, "w");
-                            fs.writeSync(fd, css, 0, "utf8");
-                            fs.closeSync(fd);
-                            next();
-                        } else {
-                            sys.print(css);
-                        }
-                    } catch (e) {
-                        less.writeError(e, lessCompileOptions);
-                        process.exit(2);
-                    }
-                }
-            });
-    }
+	function parseLessFile(e, data) {
+		if (e) {
+			sys.puts("Error to read less " + (lessCompileOptions.path + input) + " file: " + e.message);
+			process.exit(1);
+		}
+		console.dir('Compiling LESS ' + lessCompileOptions.path + input);
+		new (less.Parser)({
+			paths: [lessCompileOptions.path + path.dirname(input)],
+			optimization: lessCompileOptions.optimization,
+			filename: path.basename(input),
+			strictImports: lessCompileOptions.strictImports
+		}).parse(data, function (err, tree) {
+				if (err) {
+					less.writeError(err, lessCompileOptions);
+					process.exit(1);
+				} else {
+					try {
+						css = tree.toCSS({
+							compress: lessCompileOptions.compress,
+							yuicompress: lessCompileOptions.yuicompress
+						});
+						if (output) {
+							fd = fs.openSync(output, "w");
+							fs.writeSync(fd, css, 0, "utf8");
+							fs.closeSync(fd);
+							next();
+						} else {
+							sys.print(css);
+						}
+					} catch (e) {
+						less.writeError(e, lessCompileOptions);
+						process.exit(2);
+					}
+				}
+			});
+	}
 }
