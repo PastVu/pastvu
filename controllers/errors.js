260ccb49b915352d16431745a9b8bafa50f26a0a
klimashkin <klimashkin@gmail.com>
2013-09-23 20:36:43 +0400

    errors переведены на util.inherits

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,21 +1,22 @@
 'use strict';
 
-var logger = require('log4js').getLogger("error.js"),
+var util = require('util'),
+	logger = require('log4js').getLogger("error.js"),
 	logger404 = require('log4js').getLogger("404.js"),
 	neoError = {
-		e404: function e404(msgs) {
-			this.message = msgs;
-			Error.call(this);
+		e404: function e404(message) {
+			this.message = message;
 			Error.captureStackTrace(this, e404);
 		},
-		e500: function e500(msgs) {
-			this.message = msgs;
-			Error.call(this);
+		e500: function e500(message) {
+			this.message = message;
 			Error.captureStackTrace(this, e500);
 		}
 	};
-neoError.e404.prototype = Object.create(Error.prototype);
-neoError.e500.prototype = Object.create(Error.prototype);
+util.inherits(neoError.e404, Error);
+util.inherits(neoError.e500, Error);
+neoError.e404.prototype.name = 'HTTP404Error';
+neoError.e500.prototype.name = '500Error';
 
 function send404(req, res, err) {
 	logger404.error(JSON.stringify({url: req.url, method: req.method, ua: req.headers && req.headers['user-agent'], referer: req.headers && req.headers.referer}));
@@ -41,7 +42,7 @@ module.exports.registerErrorHandling = function (app) {
 
 	//Последний get. Если дошли сюда, значит на запрос нет обработчика
 	app.get('*', function (req, res) {
-		throw new neoError.e404();
+		throw new neoError.e404('No such resource');
 	});
 
 	//Обработчик выброшенных ошибок
