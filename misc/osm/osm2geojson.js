02f79baf4ec9a692f190b5c00872190006a84e58
klimashkin <klimashkin@gmail.com>
2013-10-14 21:29:06 +0400

    Клиентский страница leaf.html для конвертации Shapefile в GeoJSON

--- misc/osm/osm2geojson.js
+++ misc/osm/osm2geojson.js
@@ -1,12 +1,16 @@
 /*global gc:true*/
 'use strict';
 
+/**
+ * Конвертирует osm-файл (в формате xml) в GeoJSON
+ */
 var fs = require('fs'),
 	DOMParser = require('xmldom').DOMParser,
 	osm_and_geojson = require('osm-and-geojson'),
-	osmstring = fs.readFileSync('./RU-IN.osm', 'utf8'),
-	osmxml,
-	geojson;
+
+	osmstring = fs.readFileSync('./singa2.osm', 'utf8'), //Строка с оригинальным osm
+	osmxml, //Рапарсенный xml
+	geojson; //Результирующий GeoJSON
 
 console.log(typeof osmstring, osmstring.length);
 osmxml = new DOMParser().parseFromString(osmstring, 'text/xml');
@@ -14,7 +18,7 @@ console.log(typeof osmxml, osmxml.length);
 
 geojson = osm_and_geojson.osm2geojson(osmxml);
 
-fs.writeFile('./RU-IN.json', JSON.stringify(geojson), function (err) {
+fs.writeFile('./osmgeo.json', JSON.stringify(geojson), function (err) {
   if (err) throw err;
   console.log('It\'s saved!');
 });
