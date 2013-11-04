c50c240ff42247f13e4278ca7f9ab88004949869
klimashkin <klimashkin@gmail.com>
2013-11-04 19:44:18 +0400

    Миграция Bootstrap3 36. Меню нижней панели на главной

--- public/js/lib/bootstrap/collapse.js
+++ public/js/lib/bootstrap/collapse.js
@@ -21,7 +21,7 @@
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
+		define(['jquery', 'bs/transition'], factory);
 	} else {
 		// Browser globals
 		factory(jQuery);
