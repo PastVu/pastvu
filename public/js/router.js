3ac686746155bf07bae12d0c0b76cee8b08951ac
klimashkin <klimashkin@gmail.com>
2015-01-19 20:36:39 +0300

    A href attribute data-replace if we want to navigate with replaceState

--- public/js/router.js
+++ public/js/router.js
@@ -2,174 +2,179 @@
  * Менеджер путей
  */
 define(['jquery', 'underscore', 'Utils', 'knockout', 'globalVM', 'renderer'], function ($, _, Utils, ko, globalVM, renderer) {
-	"use strict";
+    "use strict";
 
-	var global = window,
-		hasHistoryApi = !!(global.history && global.history.pushState),
-		router = {
-			init: function (options) {
-				if (router.inited) {
-					console.warn('Double router init');
-					return false;
-				}
-				if (!options) {
-					options = {};
-				}
+    var global = window;
+    var hasHistoryApi = !!(global.history && global.history.pushState);
+    var router = {
+        init: function (options) {
+            if (router.inited) {
+                console.warn('Double router init');
+                return false;
+            }
+            if (!options) {
+                options = {};
+            }
 
-				router.routeChanged = ko.observable(location.href);
-				router.params = ko.observable({});
-				router.blockHrefs = false;
+            router.routeChanged = ko.observable(location.href);
+            router.params = ko.observable({});
+            router.blockHrefs = false;
 
-				if (options && options.globalModules) {
-					//Регистрируем глобальные модули
-					renderer(
-						options.globalModules.modules,
-						options.globalModules.options
-					);
-				}
+            if (options && options.globalModules) {
+                //Регистрируем глобальные модули
+                renderer(
+                    options.globalModules.modules,
+                    options.globalModules.options
+                );
+            }
 
-				//Вставляем обработчики модулей обернутые в враппер
-				if (options && options.handlers) {
-					_.forEach(options.handlers, function (item, key) {
-						router.addHandler(key, item);
-					});
-				}
+            //Вставляем обработчики модулей обернутые в враппер
+            if (options && options.handlers) {
+                _.forEach(options.handlers, function (item, key) {
+                    router.addHandler(key, item);
+                });
+            }
 
-				//Регистрируем переданные модули
-				if (options && options.routes) {
-					options.routes.forEach(function (item) {
-						router.addRoute(item.route, item.handler);
-					});
-				}
+            //Регистрируем переданные модули
+            if (options && options.routes) {
+                options.routes.forEach(function (item) {
+                    router.addRoute(item.route, item.handler);
+                });
+            }
 
-				if (hasHistoryApi) {
-					$(document).on('click', 'a', {prefix: '', body: 'route'}, router.ahrefHandler);
-					$(global).on('popstate', router.triggerUrl); //Only triggered by doing a browser action such as a clicking on the back button (or calling history.back())
-				}
-				router.inited = true;
-				return router;
-			},
-			start: function () {
-				router.triggerUrl();
-			},
+            if (hasHistoryApi) {
+                $(document).on('click', 'a', { prefix: '', body: 'route' }, router.ahrefHandler);
+                $(global).on('popstate', router.triggerUrl); //Only triggered by doing a browser action such as a clicking on the back button (or calling history.back())
+            }
+            router.inited = true;
+            return router;
+        },
+        start: function () {
+            router.triggerUrl();
+        },
 
-			routes: [],
-			handlers: {},
-			addRoute: function (route, handler, ctx) {
-				router.routes.push({route: route, handler: handler, ctx: ctx});
-			},
-			addHandler: function (name, func) {
-				router.handlers[name] = _.wrap(func, router.handlerWrapper);
-			},
-			handlerWrapper: function (handler) {
-				handler.apply(null, Array.prototype.slice.call(arguments, 1));
-			},
+        routes: [],
+        handlers: {},
+        addRoute: function (route, handler, ctx) {
+            router.routes.push({ route: route, handler: handler, ctx: ctx });
+        },
+        addHandler: function (name, func) {
+            router.handlers[name] = _.wrap(func, router.handlerWrapper);
+        },
+        handlerWrapper: function (handler) {
+            handler.apply(null, Array.prototype.slice.call(arguments, 1));
+        },
 
-			navigate: function (url, options) {
-				if (hasHistoryApi) {
-					if (!options) {
-						options = {};
-					}
-					global.history[options.replace ? 'replaceState' : 'pushState']({}, null, url);
-					if (options.trigger !== false) {
-						if (!router.triggerUrl()) {
-							global.location.reload(); //Если triggerUrl не нашел обработчиков, просто рефрешим с новым url
-						}
-					}
-				} else {
-					global.location = url;
-				}
-			},
+        navigate: function (url, options) {
+            if (hasHistoryApi) {
+                if (!options) {
+                    options = {};
+                }
+                global.history[options.replace ? 'replaceState' : 'pushState']({}, null, url);
+                if (options.trigger !== false) {
+                    if (!router.triggerUrl()) {
+                        global.location.reload(); //Если triggerUrl не нашел обработчиков, просто рефрешим с новым url
+                    }
+                }
+            } else {
+                global.location = url;
+            }
+        },
 
-			triggerUrl: function () {
-				var qparams = Utils.getURLParameters(location.href),
-					pathname = location.pathname,
-					matchedArgs,
-					handler,
-					triggered = false;
+        triggerUrl: function () {
+            var qparams = Utils.getURLParameters(location.href);
+            var pathname = location.pathname;
+            var matchedArgs;
+            var handler;
+            var triggered = false;
 
-				router.routes.forEach(function (item) {
-					if (item.route.test(pathname)) {
-						handler = router.handlers[item.handler];
-						if (_.isFunction(handler)) {
-							matchedArgs = _.chain(pathname.match(item.route)).toArray().rest().value();
-							matchedArgs.push(qparams);
-							handler.apply(item.ctx, matchedArgs);
-							triggered = true;
-						}
-					}
-				});
-				router.routeChanged(location.href);
-				return triggered;
-			},
-			checkUrl: function (pathname) {
-				if (!pathname) {
-					return false;
-				}
-				return _.some(router.routes, function (item) {
-					return item.route.test(pathname) && _.isFunction(router.handlers[item.handler]);
-				});
-			},
+            router.routes.forEach(function (item) {
+                if (item.route.test(pathname)) {
+                    handler = router.handlers[item.handler];
+                    if (_.isFunction(handler)) {
+                        matchedArgs = _.chain(pathname.match(item.route)).toArray().rest().value();
+                        matchedArgs.push(qparams);
+                        handler.apply(item.ctx, matchedArgs);
+                        triggered = true;
+                    }
+                }
+            });
+            router.routeChanged(location.href);
+            return triggered;
+        },
+        checkUrl: function (pathname) {
+            if (!pathname) {
+                return false;
+            }
+            return _.some(router.routes, function (item) {
+                return item.route.test(pathname) && _.isFunction(router.handlers[item.handler]);
+            });
+        },
 
-			// Глобальный обработчик клика по ссылке
-			ahrefHandler: function (evt) {
-				if (!hasHistoryApi) {
-					return;
-				}
-				var target = this.getAttribute('target'),
-					href = this.getAttribute('href'),
-					hrefCurrent = location.href,
-					paramsVals,
-					paramsValsCurrent,
-					paramsStringNew;
+        // Глобальный обработчик клика по ссылке
+        ahrefHandler: function (evt) {
+            if (!hasHistoryApi) {
+                return;
+            }
 
-				if (!href || href.length === 0 || router.blockHrefs) {
-					evt.preventDefault();
-				} else if (target !== '_blank' && !evt.ctrlKey && !evt.shiftKey && !evt.altKey && !evt.metaKey) {
-					//target === '_blank' --> Open a link in a new tab in foreground.
-					//Ctrl/Cmd + Shift + Click --> Open a link in a new tab in foreground.
-					//Ctrl/Cmd + Click --> Open a link in a new tab in backgroud.
-					//Shift + Click --> Open a link in a new window.
-					//Alt + Click --> Save the target on disk (open the Save As dialog).
+            var target = this.getAttribute('target');
+            var href = this.getAttribute('href');
+            var hrefCurrent = location.href;
+            var replaceState;
+            var paramsVals;
+            var paramsValsCurrent;
+            var paramsStringNew;
 
-					if (href.indexOf('?') === 0 && href.indexOf('=') > 0) {
-						//Если весь href состоит только из параметров '?x=1&y=1'
+            if (!href || href.length === 0 || router.blockHrefs) {
+                evt.preventDefault();
+            } else if (target !== '_blank' && !evt.ctrlKey && !evt.shiftKey && !evt.altKey && !evt.metaKey) {
+                //target === '_blank' --> Open a link in a new tab in foreground.
+                //Ctrl/Cmd + Shift + Click --> Open a link in a new tab in foreground.
+                //Ctrl/Cmd + Click --> Open a link in a new tab in backgroud.
+                //Shift + Click --> Open a link in a new window.
+                //Alt + Click --> Save the target on disk (open the Save As dialog).
 
-						paramsVals = Utils.getURLParameters(href);
-						paramsValsCurrent = Utils.getURLParameters(hrefCurrent);
-						delete paramsValsCurrent.hl; //Удаляем во время перехода hl текущей страницы
+                // Если на элементе стоит аттрибут data-replace="true", то вызываем replaceState вместо pushState
+                replaceState = (this.dataset && this.dataset.replace || this.getAttribute('data-replace')) === 'true';
 
-						if (_.size(paramsValsCurrent)) {
-							paramsStringNew = hrefCurrent.substr(hrefCurrent.indexOf('?')) + '&';
-							_(paramsVals).forEach(function (item, key) {
-								if (paramsValsCurrent[key]) {
-									paramsStringNew = Utils.urlReplaceParameterValue(paramsStringNew, key, item);
-								}
-							});
-						} else {
-							paramsStringNew = '?';
-						}
+                if (href.indexOf('?') === 0 && href.indexOf('=') > 0) {
+                    //Если весь href состоит только из параметров '?x=1&y=1'
 
-						_(paramsVals).forEach(function (item, key) {
-							if (!paramsValsCurrent[key]) {
-								paramsStringNew += key + '=' + item + '&';
-							}
-						});
+                    paramsVals = Utils.getURLParameters(href);
+                    paramsValsCurrent = Utils.getURLParameters(hrefCurrent);
+                    delete paramsValsCurrent.hl; //Удаляем во время перехода hl текущей страницы
 
-						evt.preventDefault(); //Должен быть внутри этих if, т.к. если они не подходят должен продолжиться стандартный переход по ссылке
-						router.navigate(location.pathname + paramsStringNew.substring(0, paramsStringNew.length - 1));
-					} else if (router.checkUrl(Utils.parseUrl(href, this).pathname)) {
-						//Если у нас есть обработчик на данный url, навигируемся
-						evt.preventDefault();
-						router.navigate(href);
-					}
-				}
-			},
-			// Блокирует переход по ссылкам
-			ahrefBlock: function (flag) {
-				router.blockHrefs = _.isBoolean(flag) ? flag : !router.blockHrefs;
-			}
-		};
+                    if (_.size(paramsValsCurrent)) {
+                        paramsStringNew = hrefCurrent.substr(hrefCurrent.indexOf('?')) + '&';
+                        _(paramsVals).forEach(function (item, key) {
+                            if (paramsValsCurrent[key]) {
+                                paramsStringNew = Utils.urlReplaceParameterValue(paramsStringNew, key, item);
+                            }
+                        });
+                    } else {
+                        paramsStringNew = '?';
+                    }
 
-	return router;
+                    _(paramsVals).forEach(function (item, key) {
+                        if (!paramsValsCurrent[key]) {
+                            paramsStringNew += key + '=' + item + '&';
+                        }
+                    });
+
+                    evt.preventDefault(); // Должен быть внутри этих if, т.к. если они не подходят должен продолжиться стандартный переход по ссылке
+                    router.navigate(location.pathname + paramsStringNew.substring(0, paramsStringNew.length - 1), { replace: replaceState });
+                } else if (router.checkUrl(Utils.parseUrl(href, this).pathname)) {
+                    // Если у нас есть обработчик на данный url, навигируемся
+                    evt.preventDefault();
+                    router.navigate(href, { replace: replaceState });
+                }
+            }
+        },
+        // Блокирует переход по ссылкам
+        ahrefBlock: function (flag) {
+            router.blockHrefs = _.isBoolean(flag) ? flag : !router.blockHrefs;
+        }
+    };
+
+    return router;
 });
