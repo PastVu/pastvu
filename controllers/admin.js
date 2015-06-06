368d80d7b2b739b676ee1367d0064b6275b03aca
klimashkin <klimashkin@gmail.com>
2015-06-07 00:44:13 +0300

    Make eslint happy with admin and setting controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,314 +1,319 @@
-'use strict';
-
-var auth = require('./auth.js'),
-	_session = require('./_session.js'),
-	Settings,
-	User,
-	Counter,
-	News,
-	_ = require('lodash'),
-	step = require('step'),
-	Utils = require('../commons/Utils.js'),
-	regionController = require('./region.js'),
-	msg = {
-		deny: 'You do not have permission for this action'
-	};
-
+var auth = require('./auth.js');
+var _session = require('./_session.js');
+var Settings;
+var User;
+var Counter;
+var News;
+var _ = require('lodash');
+var step = require('step');
+var Utils = require('../commons/Utils.js');
+var regionController = require('./region.js');
+var msg = {
+        deny: 'You do not have permission for this action'
+    };
 
 function createNews(iAm, data, cb) {
-	if (!_.isObject(data)) {
-		return cb({message: 'Bad params', error: true});
-	}
+    if (!_.isObject(data)) {
+        return cb({ message: 'Bad params', error: true });
+    }
 
-	step(
-		function () {
-			Counter.increment('news', this);
-		},
-		function (err, count) {
-			if (err || !count) {
-				return cb({message: err && err.message || 'Increment comment counter error', error: true});
-			}
+    step(
+        function () {
+            Counter.increment('news', this);
+        },
+        function (err, count) {
+            if (err || !count) {
+                return cb({ message: err && err.message || 'Increment comment counter error', error: true });
+            }
 
-			var novel = new News({
-				cid: count.next,
-				user: iAm.user,
-				pdate: data.pdate,
-				tdate: data.tdate,
-				title: data.title,
-				notice: data.notice,
-				txt: data.txt
-			});
-			novel.save(this);
-		},
-		function (err, novel) {
-			if (err || !novel) {
-				return cb({message: err && err.message || 'Save error', error: true});
-			}
-			cb({news: novel});
-		}
-	);
+            var novel = new News({
+                cid: count.next,
+                user: iAm.user,
+                pdate: data.pdate,
+                tdate: data.tdate,
+                title: data.title,
+                notice: data.notice,
+                txt: data.txt
+            });
+            novel.save(this);
+        },
+        function (err, novel) {
+            if (err || !novel) {
+                return cb({ message: err && err.message || 'Save error', error: true });
+            }
+            cb({ news: novel });
+        }
+    );
 }
 function saveNews(iAm, data, cb) {
-	if (!_.isObject(data)) {
-		return cb({message: 'Bad params', error: true});
-	}
+    if (!_.isObject(data)) {
+        return cb({ message: 'Bad params', error: true });
+    }
 
-	step(
-		function () {
-			News.findOne({cid: data.cid}, this);
-		},
-		function (err, novel) {
-			if (err || !novel) {
-				return cb({message: err && err.message || 'No such news', error: true});
-			}
-			novel.pdate = data.pdate;
-			novel.tdate = data.tdate;
-			novel.title = data.title;
-			novel.notice = data.notice;
-			novel.txt = data.txt;
-			novel.nocomments = data.nocomments ? true : undefined;
-			novel.save(this);
-		},
-		function (err, novel) {
-			if (err || !novel) {
-				return cb({message: err && err.message || 'Save error', error: true});
-			}
-			cb({news: novel});
-		}
-	);
+    step(
+        function () {
+            News.findOne({ cid: data.cid }, this);
+        },
+        function (err, novel) {
+            if (err || !novel) {
+                return cb({ message: err && err.message || 'No such news', error: true });
+            }
+            novel.pdate = data.pdate;
+            novel.tdate = data.tdate;
+            novel.title = data.title;
+            novel.notice = data.notice;
+            novel.txt = data.txt;
+            novel.nocomments = data.nocomments ? true : undefined;
+            novel.save(this);
+        },
+        function (err, novel) {
+            if (err || !novel) {
+                return cb({ message: err && err.message || 'Save error', error: true });
+            }
+            cb({ news: novel });
+        }
+    );
 }
 
 function getOnlineStat(iAm, cb) {
-	if (!iAm.isAdmin) {
-		return cb({message: msg.deny, error: true});
-	}
+    if (!iAm.isAdmin) {
+        return cb({ message: msg.deny, error: true });
+    }
 
-	var usersCount = Utils.getObjectPropertyLength(_session.usLogin),
-		sessions = _session.sessConnected,
+    var usersCount = Utils.getObjectPropertyLength(_session.usLogin);
+    var sessions = _session.sessConnected;
 
-		sessUserCount = 0,
-		sessUserZeroSockCount = 0,
-		sessUserNoSockCount = 0,
-		sessAnonymCount = 0,
-		sessAnonymZeroSockCount = 0,
-		sessAnonymNoSockCount = 0,
-		sessNoSockHeaders = [],
+    var sessUserCount = 0;
+    var sessUserZeroSockCount = 0;
+    var sessUserNoSockCount = 0;
+    var sessAnonymCount = 0;
+    var sessAnonymZeroSockCount = 0;
+    var sessAnonymNoSockCount = 0;
+    var sessNoSockHeaders = [];
 
-		sessionsWaitingConnect = _session.sessWaitingConnect,
-		sessWCUserCount = 0,
-		sessWCAnonymCount = 0,
-		sessWCNoSockHeaders = [],
+    var sessionsWaitingConnect = _session.sessWaitingConnect;
+    var sessWCUserCount = 0;
+    var sessWCAnonymCount = 0;
+    var sessWCNoSockHeaders = [];
 
-		socketUserCount = 0,
-		socketAnonymCount = 0,
+    var socketUserCount = 0;
+    var socketAnonymCount = 0;
 
-		sockets,
-		isReg,
-		count,
-		i;
+    var sockets;
+    var isReg;
+    var count;
+    var i;
 
-	for (i in sessions) {
-		if (sessions[i] !== undefined) {
-			isReg = !!sessions[i].user;
-			if (isReg) {
-				sessUserCount++;
-			} else {
-				sessAnonymCount++;
-			}
-			sockets = sessions[i].sockets;
-			if (sockets) {
-				count = Object.keys(sockets).length || 0;
-				if (isReg) {
-					if (count) {
-						socketUserCount += count;
-					} else {
-						sessUserZeroSockCount++;
-					}
-				} else {
-					if (count) {
-						socketAnonymCount += count;
-					} else {
-						sessAnonymZeroSockCount++;
-					}
-				}
-			} else {
-				if (isReg) {
-					sessUserNoSockCount++;
-				} else {
-					sessAnonymNoSockCount++;
-				}
-				sessNoSockHeaders.push({stamp: sessions[i].stamp, header: (sessions[i].data && sessions[i].data.headers) || {}});
-			}
-		}
-	}
+    for (i in sessions) {
+        if (sessions[i] !== undefined) {
+            isReg = !!sessions[i].user;
+            if (isReg) {
+                sessUserCount++;
+            } else {
+                sessAnonymCount++;
+            }
+            sockets = sessions[i].sockets;
+            if (sockets) {
+                count = Object.keys(sockets).length || 0;
+                if (isReg) {
+                    if (count) {
+                        socketUserCount += count;
+                    } else {
+                        sessUserZeroSockCount++;
+                    }
+                } else {
+                    if (count) {
+                        socketAnonymCount += count;
+                    } else {
+                        sessAnonymZeroSockCount++;
+                    }
+                }
+            } else {
+                if (isReg) {
+                    sessUserNoSockCount++;
+                } else {
+                    sessAnonymNoSockCount++;
+                }
+                sessNoSockHeaders.push({ stamp: sessions[i].stamp, header: (sessions[i].data && sessions[i].data.headers) || {} });
+            }
+        }
+    }
 
-	for (i in sessionsWaitingConnect) {
-		if (sessionsWaitingConnect[i] !== undefined) {
-			isReg = !!sessionsWaitingConnect[i].user;
-			if (isReg) {
-				sessWCUserCount++;
-			} else {
-				sessWCAnonymCount++;
-			}
-			sessWCNoSockHeaders.push({stamp: sessionsWaitingConnect[i].stamp, header: (sessionsWaitingConnect[i].data && sessionsWaitingConnect[i].data.headers) || {}});
-		}
-	}
-	cb(null, {
-		all: usersCount + sessAnonymCount,
-		users: usersCount,
+    for (i in sessionsWaitingConnect) {
+        if (sessionsWaitingConnect[i] !== undefined) {
+            isReg = !!sessionsWaitingConnect[i].user;
+            if (isReg) {
+                sessWCUserCount++;
+            } else {
+                sessWCAnonymCount++;
+            }
+            sessWCNoSockHeaders.push({
+                stamp: sessionsWaitingConnect[i].stamp,
+                header: (sessionsWaitingConnect[i].data && sessionsWaitingConnect[i].data.headers) || {}
+            });
+        }
+    }
+    cb(null, {
+        all: usersCount + sessAnonymCount,
+        users: usersCount,
 
-		sessUC: sessUserCount,
-		sessUZC: sessUserZeroSockCount,
-		sessUNC: sessUserNoSockCount,
-		sessAC: sessAnonymCount,
-		sessAZC: sessAnonymZeroSockCount,
-		sessANC: sessAnonymNoSockCount,
-		sessNCHeaders: sessNoSockHeaders,
+        sessUC: sessUserCount,
+        sessUZC: sessUserZeroSockCount,
+        sessUNC: sessUserNoSockCount,
+        sessAC: sessAnonymCount,
+        sessAZC: sessAnonymZeroSockCount,
+        sessANC: sessAnonymNoSockCount,
+        sessNCHeaders: sessNoSockHeaders,
 
-		sessWCUC: sessWCUserCount,
-		sessWCAC: sessWCAnonymCount,
-		sessWCNCHeaders: sessWCNoSockHeaders,
+        sessWCUC: sessWCUserCount,
+        sessWCAC: sessWCAnonymCount,
+        sessWCNCHeaders: sessWCNoSockHeaders,
 
-		sockUC: socketUserCount,
-		sockAC: socketAnonymCount,
+        sockUC: socketUserCount,
+        sockAC: socketAnonymCount,
 
-		cusSid: Utils.getObjectPropertyLength(_session.usSid),
-		cusLogin: usersCount,
-		cusId: Utils.getObjectPropertyLength(_session.usId),
-		csessConnected: Utils.getObjectPropertyLength(_session.sessConnected),
-		csessWaitingConnect: Utils.getObjectPropertyLength(_session.sessWaitingConnect),
-		csessWaitingSelect: Utils.getObjectPropertyLength(_session.sessWaitingSelect)
-	});
+        cusSid: Utils.getObjectPropertyLength(_session.usSid),
+        cusLogin: usersCount,
+        cusId: Utils.getObjectPropertyLength(_session.usId),
+        csessConnected: Utils.getObjectPropertyLength(_session.sessConnected),
+        csessWaitingConnect: Utils.getObjectPropertyLength(_session.sessWaitingConnect),
+        csessWaitingSelect: Utils.getObjectPropertyLength(_session.sessWaitingSelect)
+    });
 }
 
 //Сохраняем права пользователя
 function saveUserCredentials(iAm, data, cb) {
-	if (!iAm.isAdmin) {
-		return cb({message: msg.deny, error: true});
-	}
+    if (!iAm.isAdmin) {
+        return cb({ message: msg.deny, error: true });
+    }
 
-	var login = data && data.login,
-		itsMe = iAm.user.login === login,
-		userObjOnline;
+    var login = data && data.login;
+    var itsMe = iAm.user.login === login;
+    var userObjOnline;
 
-	if (!_.isObject(data) || !login || data.role < 0 || data.role > 11) {
-		return cb({message: msg.badParams, error: true});
-	}
+    if (!_.isObject(data) || !login || data.role < 0 || data.role > 11) {
+        return cb({ message: msg.badParams, error: true });
+    }
 
-	if (itsMe && iAm.user.role !== data.role) {
-		return cb({message: 'Administrators can not change their role :)', error: true});
-	}
+    if (itsMe && iAm.user.role !== data.role) {
+        return cb({ message: 'Administrators can not change their role :)', error: true });
+    }
 
-	step(
-		function () {
-			userObjOnline = _session.getOnline(login);
-			if (userObjOnline) {
-				this(null, userObjOnline.user);
-			} else {
-				User.findOne({login: login}).populate('mod_regions', {_id: 0, cid: 1}).exec(this);
-			}
-		},
-		function (err, user) {
-			if (err || !user) {
-				return cb({message: err && err.message || msg.nouser, error: true});
-			}
+    step(
+        function () {
+            userObjOnline = _session.getOnline(login);
+            if (userObjOnline) {
+                this(null, userObjOnline.user);
+            } else {
+                User.findOne({ login: login }).populate('mod_regions', { _id: 0, cid: 1 }).exec(this);
+            }
+        },
+        function (err, user) {
+            if (err || !user) {
+                return cb({ message: err && err.message || msg.nouser, error: true });
+            }
 
-			if (!itsMe) {
-				if (user.role < 11 && data.role === 11) {
-					return cb({message: 'The role of the super admin can not be assigned through the user management interface', error: true});
-				}
-				if (iAm.user.role === 10 && user.role < 10 && data.role > 9) {
-					return cb({message: 'Only super administrators can assign other administrators', error: true});
-				}
-			}
-			var existsRegions;
+            if (!itsMe) {
+                if (user.role < 11 && data.role === 11) {
+                    return cb({
+                        message: 'The role of the super admin can not be assigned through the user management interface',
+                        error: true
+                    });
+                }
+                if (iAm.user.role === 10 && user.role < 10 && data.role > 9) {
+                    return cb({ message: 'Only super administrators can assign other administrators', error: true });
+                }
+            }
+            var existsRegions;
 
-			if (data.role === 5 && data.regions) {
-				existsRegions = [];
-				user.mod_regions.forEach(function (item) {
-					existsRegions.push(item.cid);
-				});
-				if (!_.isEqual(data.regions, existsRegions)) {
-					regionController.setUserRegions(login, data.regions, 'mod_regions', function (err) {
-						if (err) {
-							return cb({message: err.message, error: true});
-						}
-						if (userObjOnline) {
-							_session.regetUser(userObjOnline, false, null, function (err) {
-								if (err) {
-									return cb({message: err.message, error: true});
-								}
-								further();
-							});
-						} else {
-							further();
-						}
-					});
-				} else {
-					further();
-				}
-			} else {
-				further();
-			}
+            if (data.role === 5 && data.regions) {
+                existsRegions = [];
+                user.mod_regions.forEach(function (item) {
+                    existsRegions.push(item.cid);
+                });
+                if (!_.isEqual(data.regions, existsRegions)) {
+                    regionController.setUserRegions(login, data.regions, 'mod_regions', function (err) {
+                        if (err) {
+                            return cb({ message: err.message, error: true });
+                        }
+                        if (userObjOnline) {
+                            _session.regetUser(userObjOnline, false, null, function (err) {
+                                if (err) {
+                                    return cb({ message: err.message, error: true });
+                                }
+                                further();
+                            });
+                        } else {
+                            further();
+                        }
+                    });
+                } else {
+                    further();
+                }
+            } else {
+                further();
+            }
 
-			function further() {
-				if (user.role !== data.role) {
-					user.role = data.role || undefined;
-					if (data.role !== 5) {
-						user.mod_regions = undefined;
-					}
-				}
+            function further() {
+                if (user.role !== data.role) {
+                    user.role = data.role || undefined;
+                    if (data.role !== 5) {
+                        user.mod_regions = undefined;
+                    }
+                }
 
-				user.save(function (err, savedUser) {
-					if (err) {
-						return cb({message: err.message, error: true});
-					}
+                user.save(function (err) {
+                    if (err) {
+                        return cb({ message: err.message, error: true });
+                    }
 
-					if (userObjOnline) {
-						_session.emitUser(userObjOnline);
-					}
-					cb({message: 'ok', saved: true});
-				});
-			}
-		}
-	);
+                    if (userObjOnline) {
+                        _session.emitUser(userObjOnline);
+                    }
+                    cb({ message: 'ok', saved: true });
+                });
+            }
+        }
+    );
 }
-
 
 module.exports.loadController = function (app, db, io) {
 
-	Settings = db.model('Settings');
-	Counter = db.model('Counter');
-	User = db.model('User');
-	News = db.model('News');
+    Settings = db.model('Settings');
+    Counter = db.model('Counter');
+    User = db.model('User');
+    News = db.model('News');
 
-	io.sockets.on('connection', function (socket) {
-		var hs = socket.handshake;
+    io.sockets.on('connection', function (socket) {
+        var hs = socket.handshake;
 
-		socket.on('saveNews', function (data) {
-			if (!hs.usObj.isAdmin) {
-				return result ({message: msg.deny, error: true});
-			}
-			if (data.cid) {
-				saveNews(hs.usObj, data, result);
-			} else {
-				createNews(hs.usObj, data, result);
-			}
-			function result (resultData) {
-				socket.emit('saveNewsResult', resultData);
-			}
-		});
+        socket.on('saveNews', function (data) {
+            if (!hs.usObj.isAdmin) {
+                return result({ message: msg.deny, error: true });
+            }
+            if (data.cid) {
+                saveNews(hs.usObj, data, result);
+            } else {
+                createNews(hs.usObj, data, result);
+            }
+            function result(resultData) {
+                socket.emit('saveNewsResult', resultData);
+            }
+        });
 
-		socket.on('getOnlineStat', function () {
-			getOnlineStat(hs.usObj, function (err, resultData) {
-				socket.emit('takeOnlineStat', resultData);
-			});
-		});
-		socket.on('saveUserCredentials', function (data) {
-			saveUserCredentials(hs.usObj, data, function (resultData) {
-				socket.emit('saveUserCredentialsResult', resultData);
-			});
-		});
-	});
+        socket.on('getOnlineStat', function () {
+            getOnlineStat(hs.usObj, function (err, resultData) {
+                if (err) {
+                    console.log(err);
+                }
+                socket.emit('takeOnlineStat', resultData);
+            });
+        });
+        socket.on('saveUserCredentials', function (data) {
+            saveUserCredentials(hs.usObj, data, function (resultData) {
+                socket.emit('saveUserCredentialsResult', resultData);
+            });
+        });
+    });
 
 };
--- controllers/settings.js
+++ controllers/settings.js
@@ -1,134 +1,132 @@
-'use strict';
+var Settings;
+var UserSettingsDef;
+var step = require('step');
+var logger = require('log4js').getLogger('settings.js');
+var appvar;
+var appEnv = {};
 
-var Settings,
-	UserSettingsDef,
-	step = require('step'),
-	logger = require('log4js').getLogger("settings.js"),
-	appvar,
-	appEnv = {},
-
-	clientSettings,
-	userSettingsDef,
-	userSettingsVars = {},
-	userRanks,
-	userRanksHash = {};
+var clientSettings;
+var userSettingsDef;
+var userSettingsVars = {};
+var userRanks;
+var userRanksHash = {};
 
 /**
  * Заполняем объект для параметров клиента
  */
 function fillClientParams(cb) {
-	var params = {
-		server: appEnv.serverAddr,
-		appHash: appEnv.hash,
-		appVersion: appEnv.version
-	};
-	step(
-		function () {
-			Settings.find({}, {_id: 0, key: 1, val: 1}, {lean: true}, this);
-		},
-		function (err, settings) {
-			if (err) {
-				logger.error(err);
-				return cb(err);
-			}
-			for (var i = settings.length; i--;) {
-				params[settings[i].key] = settings[i].val;
-			}
-			clientSettings = params;
-			cb();
-		}
-	);
+    var params = {
+        server: appEnv.serverAddr,
+        appHash: appEnv.hash,
+        appVersion: appEnv.version
+    };
+    step(
+        function () {
+            Settings.find({}, { _id: 0, key: 1, val: 1 }, { lean: true }, this);
+        },
+        function (err, settings) {
+            if (err) {
+                logger.error(err);
+                return cb(err);
+            }
+            for (var i = 0; i < settings.length; i++) {
+                params[settings[i].key] = settings[i].val;
+            }
+            clientSettings = params;
+            cb();
+        }
+    );
 }
 
 /**
  * Заполняем объект для параметров пользователя по умолчанию
  */
 function fillUserSettingsDef(cb) {
-	var params = {};
-	step(
-		function () {
-			UserSettingsDef.find({key: {$ne: 'ranks'}}, {_id: 0, key: 1, val: 1, vars: 1}, {lean: true}, this);
-		},
-		function (err, settings) {
-			if (err) {
-				logger.error(err);
-				return cb(err);
-			}
-			for (var i = settings.length; i--;) {
-				params[settings[i].key] = settings[i].val;
-				userSettingsVars[settings[i].key] = settings[i].vars;
-			}
-			userSettingsDef = params;
-			cb();
-		}
-	);
+    var params = {};
+    step(
+        function () {
+            UserSettingsDef.find({ key: { $ne: 'ranks' } }, { _id: 0, key: 1, val: 1, vars: 1 }, { lean: true }, this);
+        },
+        function (err, settings) {
+            if (err) {
+                logger.error(err);
+                return cb(err);
+            }
+            for (var i = 0; i < settings.length; i++) {
+                params[settings[i].key] = settings[i].val;
+                userSettingsVars[settings[i].key] = settings[i].vars;
+            }
+            userSettingsDef = params;
+            cb();
+        }
+    );
 }
 
 /**
  * Заполняем объект для возможных званий пользователя
  */
 function fillUserRanks(cb) {
-	step(
-		function () {
-			UserSettingsDef.findOne({key: 'ranks'}, {_id: 0, vars: 1}, {lean: true}, this);
-		},
-		function (err, row) {
-			if (err) {
-				logger.error(err);
-				return cb(err);
-			}
-			for (var i = 0; i < row.vars.length; i++) {
-				userRanksHash[row.vars[i]] = 1;
-			}
-			userRanks = row.vars;
-			cb();
-		}
-	);
+    step(
+        function () {
+            UserSettingsDef.findOne({ key: 'ranks' }, { _id: 0, vars: 1 }, { lean: true }, this);
+        },
+        function (err, row) {
+            if (err) {
+                logger.error(err);
+                return cb(err);
+            }
+            for (var i = 0; i < row.vars.length; i++) {
+                userRanksHash[row.vars[i]] = 1;
+            }
+            userRanks = row.vars;
+            cb();
+        }
+    );
 }
 
 module.exports.getClientParams = function () {
-	return clientSettings;
+    return clientSettings;
 };
 module.exports.getUserSettingsDef = function () {
-	return userSettingsDef;
+    return userSettingsDef;
 };
 module.exports.getUserSettingsVars = function () {
-	return userSettingsVars;
+    return userSettingsVars;
 };
 module.exports.getUserRanks = function () {
-	return userRanks;
+    return userRanks;
 };
 module.exports.getUserRanksHash = function () {
-	return userRanksHash;
+    return userRanksHash;
 };
 
 module.exports.loadController = function (app, db, io, cb) {
-	appvar = app;
-	appEnv = app.get('appEnv');
+    appvar = app;
+    appEnv = app.get('appEnv');
 
-	Settings = db.model('Settings');
-	UserSettingsDef = db.model('UserSettingsDef');
-	step(
-		function () {
-			fillClientParams(this.parallel());
-			fillUserSettingsDef(this.parallel());
-			fillUserRanks(this.parallel());
-		},
-		function (err) {
-			io.sockets.on('connection', function (socket) {
-				socket.on('giveClientParams', function () {
-					socket.emit('takeClientParams', clientSettings);
-				});
+    Settings = db.model('Settings');
+    UserSettingsDef = db.model('UserSettingsDef');
+    step(
+        function () {
+            fillClientParams(this.parallel());
+            fillUserSettingsDef(this.parallel());
+            fillUserRanks(this.parallel());
+        },
+        function (err) {
+            io.sockets.on('connection', function (socket) {
+                socket.on('giveClientParams', function () {
+                    socket.emit('takeClientParams', clientSettings);
+                });
 
-				socket.on('giveUserSettingsVars', function () {
-					socket.emit('takeUserSettingsVars', userSettingsVars);
-				});
+                socket.on('giveUserSettingsVars', function () {
+                    socket.emit('takeUserSettingsVars', userSettingsVars);
+                });
 
-				socket.on('giveUserAllRanks', function () {
-					socket.emit('takeUserAllRanks', userRanks);
-				});
-			});
-			cb(err);
-		}
-	);
+                socket.on('giveUserAllRanks', function () {
+                    socket.emit('takeUserAllRanks', userRanks);
+                });
+            });
+            cb(err);
+        }
+    );
 };
