e6f104e65b9fc5b33d273b75e9e728d0dd942313
klimashkin <klimashkin@gmail.com>
2015-09-15 16:27:26 -0700

    Serve out metadata (and opengraph) on photo page

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,120 +1,183 @@
 'use strict';
 
-var _ = require('lodash'),
-	Utils = require('../commons/Utils.js'),
-	settings = require('./settings.js'),
-	_session = require('./_session.js');
+var _ = require('lodash');
+var Utils = require('../commons/Utils');
+var settings = require('./settings');
+var _session = require('./_session');
+var photo = require('./photo');
 
 module.exports.loadController = function (app) {
-	var genInitDataString = (function () {
-			var clientParamsJSON = JSON.stringify(settings.getClientParams());
-			return function (usObj) {
-				var resultString = 'var init={settings:' + clientParamsJSON + ', user:' + JSON.stringify(_session.getPlainUser(usObj.user));
+    var clientParams = settings.getClientParams();
+    var fullhost = clientParams.server.protocol + '://' + clientParams.server.host;
 
-				if (usObj.registered) {
-					resultString += ',registered:true';
-				}
+    var genInitDataString = (function () {
+        var clientParamsJSON = JSON.stringify(clientParams);
 
-				resultString += '};';
+        return function (req) {
+            var usObj = req.handshake.usObj;
+            var resultString = 'var init={settings:' + clientParamsJSON + ', user:' + JSON.stringify(_session.getPlainUser(usObj.user));
 
-				return resultString;
-			};
-		}()),
+            if (usObj.registered) {
+                resultString += ',registered:true';
+            }
 
-	//Проверка на выключенный у клиенты js. В этом случае клиент передаст параметр _nojs=1 в url
-		checkNoJS = function (req) {
-			var nojsShow = req.query._nojs === '1',
-				nojsUrl;
+            if (req.photoData) {
+                resultString += ',photo:' + JSON.stringify(req.photoData);
+            }
 
-			//Если страница уже не для "отсутствует javascript", вставляем в noscript ссылку на редирект в случае отсутствия javascript
-			if (!nojsShow) {
-				nojsUrl = req._parsedUrl.pathname + '?' + (req._parsedUrl.query ? req._parsedUrl.query + '&' : '') + '_nojs=1';
-			}
-			return {nojsUrl: nojsUrl, nojsShow: nojsShow};
-		},
+            resultString += '};';
 
-	//Для путей, которым не нужна установка сессии напрямую парсим браузер
-		getReqBrowser = function (req, res, next) {
-			var ua = req.headers['user-agent'];
-			if (ua) {
-				req.browser = _session.checkUserAgent(ua);
-			}
-			next();
-		},
+            return resultString;
+        };
+    }());
 
-	//Заполняем некоторые заголовки для полностью генерируемых страниц
-		setStaticHeaders = (function () {
-			var cacheControl = 'no-cache',
-				xFramePolicy = 'SAMEORIGIN',
-				xPoweredBy = 'Paul Klimashkin | klimashkin@gmail.com',
-				xUA = 'IE=edge';
+    // Проверка на выключенный у клиенты js. В этом случае клиент передаст параметр _nojs=1 в url
+    var checkNoJS = function (req) {
+        var nojsShow = req.query._nojs === '1';
+        var nojsUrl;
 
-			return function (req, res, next) {
-				//Директива ответа для указания браузеру правила кеширования
-				//no-cache - браузеру и прокси разрешено кешировать, с обязательным запросом актуальности
-				//(в случае с наличием etag в первом ответе, в следующем запросе клиент для проверки актуальности передаст этот etag в заголовке If-None-Match)
-				res.setHeader('Cache-Control', cacheControl);
+        //Если страница уже не для "отсутствует javascript", вставляем в noscript ссылку на редирект в случае отсутствия javascript
+        if (!nojsShow) {
+            nojsUrl = req._parsedUrl.pathname + '?' + (req._parsedUrl.query ? req._parsedUrl.query + '&' : '') + '_nojs=1';
+        }
+        return { nojsUrl: nojsUrl, nojsShow: nojsShow };
+    };
 
-				//The page can only be displayed in a frame on the same origin as the page itself https://developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options
-				res.setHeader('X-Frame-Options', xFramePolicy);
+    // Для путей, которым не нужна установка сессии напрямую парсим браузер
+    var getReqBrowser = function (req, res, next) {
+        var ua = req.headers['user-agent'];
+        if (ua) {
+            req.browser = _session.checkUserAgent(ua);
+        }
+        next();
+    };
 
-				if (req.browser && req.browser.agent.family === 'IE') {
-					//X-UA-Compatible header has greater precedence than Compatibility View http://msdn.microsoft.com/en-us/library/ff955275(v=vs.85).aspx
-					res.setHeader('X-UA-Compatible', xUA);
-				}
+    // Заполняем некоторые заголовки для полностью генерируемых страниц
+    var setStaticHeaders = (function () {
+        var cacheControl = 'no-cache',
+            xFramePolicy = 'SAMEORIGIN',
+            xPoweredBy = 'Paul Klimashkin | klimashkin@gmail.com',
+            xUA = 'IE=edge';
 
-				res.setHeader('X-Powered-By', xPoweredBy);
-				if (typeof next === 'function') {
-					next();
-				}
-			};
-		}());
+        return function (req, res, next) {
+            // Директива ответа для указания браузеру правила кеширования
+            // no-cache - браузеру и прокси разрешено кешировать, с обязательным запросом актуальности
+            // (в случае с наличием etag в первом ответе, в следующем запросе клиент для проверки актуальности передаст этот etag в заголовке If-None-Match)
+            res.setHeader('Cache-Control', cacheControl);
 
-	[
-		'/', //Корень
-		/^\/(?:photoUpload)\/?$/, // Пути строгие (/example без или с завершающим слешом)
-		/^\/(?:ps|u|news)(?:\/.*)?$/, // Пути с возможным продолжением (/example/*)
-		/^\/(?:p|confirm)\/.+$/ // Пути обязательным продолжением (/example/*)
-	]
-		.forEach(function (route) {
-			app.get(route, _session.handleHTTPRequest, setStaticHeaders, appMainHandler);
-		});
-	function appMainHandler(req, res) {
-		var nojs = checkNoJS(req);
-		res.statusCode = 200;
-		res.render('app', {appName: 'Main', initData: genInitDataString(req.handshake.usObj), nojsUrl: nojs.nojsUrl, nojsShow: nojs.nojsShow, agent: req.browser && req.browser.agent});
-	}
+            // The page can only be displayed in a frame on the same origin as the page itself https://developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options
+            res.setHeader('X-Frame-Options', xFramePolicy);
 
+            if (req.browser && req.browser.agent.family === 'IE') {
+                // X-UA-Compatible header has greater precedence than Compatibility View http://msdn.microsoft.com/en-us/library/ff955275(v=vs.85).aspx
+                res.setHeader('X-UA-Compatible', xUA);
+            }
 
-	[/^\/(?:admin)(?:\/.*)?$/].forEach(function (route) {
-		app.get(route, _session.handleHTTPRequest, setStaticHeaders, appAdminHandler);
-	});
-	function appAdminHandler(req, res) {
-		var nojs = checkNoJS(req);
-		res.statusCode = 200;
-		res.render('app', {appName: 'Admin', initData: genInitDataString(req.handshake.usObj), nojsUrl: nojs.nojsUrl, nojsShow: nojs.nojsShow, agent: req.browser && req.browser.agent});
-	}
+            res.setHeader('X-Powered-By', xPoweredBy);
+            if (typeof next === 'function') {
+                next();
+            }
+        };
+    }());
 
+    [
+        '/', // Корень
+        /^\/(?:photoUpload)\/?$/, // Пути строгие (/example без или с завершающим слешом)
+        /^\/(?:ps|u|news)(?:\/.*)?$/, // Пути с возможным продолжением (/example/*)
+        /^\/(?:confirm)\/.+$/ // Пути обязательным продолжением (/example/*)
+    ]
+        .forEach(function (route) {
+            app.get(route, _session.handleHTTPRequest, setStaticHeaders, appMainHandler);
+        });
 
-	//Устаревший браузер
-	app.get('/badbrowser', getReqBrowser, setStaticHeaders, function (req, res) {
-		res.statusCode = 200;
-		res.render('status/badbrowser', {agent: req.browser && req.browser.agent, title: 'Вы используете устаревшую версию браузера'});
-	});
+    app.get(/^\/p\/(\d{1,7})$/, _session.handleHTTPRequest, setStaticHeaders, getPhotoForPage, appMainHandler);
 
-	//Мой user-agent
-	app.get('/myua', getReqBrowser, function (req, res) {
-		res.setHeader('Cache-Control', 'no-cache,no-store,max-age=0,must-revalidate');
-		res.statusCode = 200;
-		res.render('status/myua', {
-			agent: req.browser && req.browser.agent,
-			accept: req.browser && req.browser.accept,
-			title: req.browser && req.browser.agent && req.browser.agent.source
-		});
-	});
+    function appMainHandler(req, res) {
+        var nojs = checkNoJS(req);
+        var photo = req.photoData && req.photoData.photo;
 
-	//ping-pong для проверки работы сервера
-	app.all('/ping', function (req, res) {
-		res.status(200).send('pong');
-	});
+        var meta = { og: {} };
+
+        if (photo) {
+            meta.title = meta.og.title = photo.title;
+            meta.desc = meta.og.desc = photo.desc;
+            meta.og.img = {
+                url: fullhost + '/_p/a/' + photo.file,
+                w: photo.w,
+                h: photo.h
+            };
+        }
+        if (!meta.title) {
+            meta.title = meta.og.title = 'PastVu - retro photos of mankind\'s habitat.';
+        }
+        if (!meta.desc) {
+            meta.desc = meta.og.desc = 'Archive of historical photos, generated by users';
+        }
+
+        res.statusCode = 200;
+        res.render('app', {
+            appName: 'Main',
+            initData: genInitDataString(req),
+            meta: meta,
+            nojsUrl: nojs.nojsUrl,
+            nojsShow: nojs.nojsShow,
+            agent: req.browser && req.browser.agent
+        });
+    }
+
+    function getPhotoForPage(req, res, next) {
+        var cid = Number(req.params[0]);
+
+        photo.givePhotoForPage(req.handshake.usObj, { cid: cid })
+            .then(function (result) {
+                if (!result) {
+                    return next({ message: 'Photo ' + cid + ' does not exist' });
+                }
+
+                req.photoData = result;
+                next();
+            });
+    }
+
+    [/^\/(?:admin)(?:\/.*)?$/].forEach(function (route) {
+        app.get(route, _session.handleHTTPRequest, setStaticHeaders, appAdminHandler);
+    });
+    function appAdminHandler(req, res) {
+        var nojs = checkNoJS(req);
+
+        res.statusCode = 200;
+        res.render('app', {
+            appName: 'Admin',
+            initData: genInitDataString(req),
+            meta: {},
+            nojsUrl: nojs.nojsUrl,
+            nojsShow: nojs.nojsShow,
+            agent: req.browser && req.browser.agent
+        });
+    }
+
+    // Устаревший браузер
+    app.get('/badbrowser', getReqBrowser, setStaticHeaders, function (req, res) {
+        res.statusCode = 200;
+        res.render('status/badbrowser', {
+            agent: req.browser && req.browser.agent,
+            title: 'Вы используете устаревшую версию браузера'
+        });
+    });
+
+    // Мой user-agent
+    app.get('/myua', getReqBrowser, function (req, res) {
+        res.setHeader('Cache-Control', 'no-cache,no-store,max-age=0,must-revalidate');
+        res.statusCode = 200;
+        res.render('status/myua', {
+            agent: req.browser && req.browser.agent,
+            accept: req.browser && req.browser.accept,
+            title: req.browser && req.browser.agent && req.browser.agent.source
+        });
+    });
+
+    // ping-pong для проверки работы сервера
+    app.all('/ping', function (req, res) {
+        res.status(200).send('pong');
+    });
 };
