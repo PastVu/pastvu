1f988890f4bffdfa2d1e7b39ef34930ec3b173f9
klimashkin <klimashkin@gmail.com>
2015-10-01 19:38:40 -0700

    transform middleware to es6

--- bin/run.js
+++ bin/run.js
@@ -35,7 +35,7 @@ function babelRequire(modulePath) {
 
     // Use require-hook babel without it's runtime
     require('babel-core/register')({
-        only: /(?:app|photoConverter|downloader)\.js/,
+        only: /(?:app|photoConverter|downloader|middleware)\.js/,
         stage: 0,
         // whitelist: [],
         blacklist: [
--- controllers/middleware.js
+++ controllers/middleware.js
@@ -1,68 +1,67 @@
 'use strict';
 
-var url = require('url'),
-	path = require('path'),
-	Utils = require('../commons/Utils.js');
+import path from 'path';
+import Utils from '../commons/Utils';
 
-//Middleware для проверки запрашиваемого html, есть ли такой jade,
-//если да - компиляция, нет - передаем следующему обработчику
-module.exports.jadeToHtml = function (seekPath) {
-	var htmlRegExp = /\.html$/;
+// Middleware для проверки запрашиваемого html, есть ли такой jade,
+// если да - компиляция, нет - передаем следующему обработчику
+export function jadeToHtml(seekPath) {
+    const htmlRegExp = /\.html$/;
 
-	return function (req, res, next) {
-		if (req.method.toUpperCase() !== 'GET' && req.method.toUpperCase() !== 'HEAD') {
-			return next();
-		}
+    return function (req, res, next) {
+        if (req.method.toUpperCase() !== 'GET' && req.method.toUpperCase() !== 'HEAD') {
+            return next();
+        }
 
-		var pathname = req.path, //Getter for url.parse(req.url).pathname,
-			jadePath;
+        const pathname = req.path; // Getter for url.parse(req.url).pathname,
 
-		// Only handle the matching files
-		if (htmlRegExp.test(pathname)) {
-			jadePath = path.normalize(seekPath + (pathname.replace('.html', '.jade')));
+        // Only handle the matching files
+        if (htmlRegExp.test(pathname)) {
+            const jadePath = path.normalize(seekPath + (pathname.replace('.html', '.jade')));
 
-			res.render(jadePath, {}, function (err, renderedHTML) {
-				if (err || !renderedHTML) {
-					next();
-				} else {
-					console.log('%s compiled from jade', req.url);
-					res.status(200).send(renderedHTML);
-				}
-			});
-		} else {
-			return next();
-		}
-	};
+            res.render(jadePath, {}, function (err, renderedHTML) {
+                if (err || !renderedHTML) {
+                    next();
+                } else {
+                    console.log('%s compiled from jade', req.url);
+                    res.status(200).send(renderedHTML);
+                }
+            });
+        } else {
+            return next();
+        }
+    };
 };
 
-//Middleware для включения cors для переданного домена и всех поддоменов
-module.exports.cors = function (originRoot) {
-	var originRegExp = new RegExp(originRoot + '$', '');
+// Middleware для включения cors для переданного домена и всех поддоменов
+export function cors(originRoot) {
+    const originRegExp = new RegExp(originRoot + '$', '');
 
-	return function (req, res, next) {
-		var origin = req.headers.origin || req.headers.Origin;
-		if (origin && originRegExp.test(origin)) {
-			res.setHeader("Access-Control-Allow-Origin", origin);
-			res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-			res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-		}
-		next();
-	};
+    return function (req, res, next) {
+        const origin = req.headers.origin || req.headers.Origin;
+
+        if (origin && originRegExp.test(origin)) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+            res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        }
+        next();
+    };
 };
 
-//Добавляет заголовок X-Response-Time
-module.exports.responseHeaderHook = function () {
-	return function (req, res, next) {
-		var start = Date.now(),
-			writeHeadOriginal = res.writeHead;
+// Добавляет заголовок X-Response-Time
+export function responseHeaderHook() {
+    return function (req, res, next) {
+        const start = Date.now();
+        const writeHeadOriginal = res.writeHead;
 
-		if (!next) {
-			next = Utils.dummyFn;
-		}
-		res.writeHead = function () {
-			res.setHeader('X-Response-Time', (Date.now() - start) + 'ms');
-			writeHeadOriginal.apply(res, arguments);
-		};
-		next();
-	};
+        if (!next) {
+            next = Utils.dummyFn;
+        }
+        res.writeHead = function () {
+            res.setHeader('X-Response-Time', (Date.now() - start) + 'ms');
+            writeHeadOriginal.apply(res, arguments);
+        };
+        next();
+    };
 };
