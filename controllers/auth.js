433c266afb6905f6eb85288074fe173f43089012
klimashkin <klimashkin@gmail.com>
2015-10-31 22:33:52 -0700

    Auth controller refactored to es7

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import ms from 'ms';
 import _ from 'lodash';
 import path from 'path';
-import step from 'step';
 import jade from 'jade';
 import log4js from 'log4js';
 import moment from 'moment';
@@ -20,18 +19,32 @@ moment.locale(config.lang);
 
 const ms2d = ms('2d');
 const human2d = moment.duration(ms2d).humanize();
-
 const logger = log4js.getLogger('auth.js');
-const preaddrs = config.client.subdomains.map(function (sub) {
-    return `${sub}.${config.client.host}`;
-});
 const msg = {
     deny: 'You do not have permission for this action',
-    regError: 'Ошибка регистрации'
+    regError: 'Ошибка регистрации',
+    recError: 'Ошибка восстановления пароля',
+    passChangeError: 'Ошибка смены пароля'
 };
 
 let recallTpl;
 let regTpl;
+
+export const ready = new Promise(async function(resolve, reject) {
+    try {
+        const [regData, recallData] = await* [
+            fs.readFileAsync(path.normalize('./views/mail/registration.jade'), 'utf-8'),
+            fs.readFileAsync(path.normalize('./views/mail/recall.jade'), 'utf-8')
+        ];
+
+        regTpl = jade.compile(regData, { filename: path.normalize('./views/mail/registration.jade'), pretty: false });
+        recallTpl = jade.compile(recallData, { filename: path.normalize('./views/mail/recall.jade'), pretty: false });
+        resolve();
+    } catch (err) {
+        err.message = 'Auth jade read error: ' + err.message;
+        reject(err);
+    }
+});
 
 // Users login
 async function login(socket, { login, pass } = {}) {
@@ -45,7 +58,7 @@ async function login(socket, { login, pass } = {}) {
     try {
         const user = await User.getAuthenticated(login, pass);
 
-        // Pass user to session
+        // Transfer user to session
         const { userPlain } = await session.loginUser(socket, user);
 
         return { message: 'Success login', youAre: userPlain };
@@ -75,7 +88,7 @@ async function logout(socket) {
 };
 
 // Registration
-async function register(iAm, { login, email, pass, pass2 }) {
+async function register(iAm, { login, email, pass, pass2 } = {}) {
     if (!login) {
         throw { message: 'Заполните имя пользователя' };
     }
@@ -160,7 +173,7 @@ async function register(iAm, { login, email, pass, pass2 }) {
         });
 
     } catch (err) {
-        await User.remove({ login });
+        await User.remove({ login }).exec();
 
         logger.error('Auth register after save: ', err);
         throw { message: msg.regError };
@@ -172,243 +185,141 @@ async function register(iAm, { login, email, pass, pass2 }) {
     };
 }
 
-// Отправка на почту запроса на восстановление пароля
-var successPublic = { message: 'Запрос успешно отправлен. Для продолжения процедуры следуйте инструкциям, высланным на Ваш e-mail' }, //success = 'The data is successfully sent. To restore password, follow the instructions sent to Your e-mail',
-    recallPublicError = { message: 'Ошибка восстановления пароля', error: true };
-function recall(iAm, data, cb) {
-    var confirmKey = '';
-
-    if (!_.isObject(data) || !data.login) {
-        return cb({ message: 'Bad params', error: true });
+// Send to email request for password recovery
+async function recall(iAm, { login } = {}) {
+    if (!login || !_.isString(login)) {
+        throw { message: 'Bad params' };
     }
 
-    step(
-        function checkUserExists() {
-            User.findOne({
-                $or: [
-                    { login: new RegExp('^' + data.login + '$', 'i') },
-                    { email: data.login.toLowerCase() }
-                ]
-            }).exec(this);
-        },
-        function (err, user) {
-            if (err) {
-                logger.error('Auth recall User.findOne: ', err);
-                return cb(recallPublicError);
-            }
-            if (!user) {
-                return cb({ message: 'Пользователя с таким логином или e-mail не существует', error: true }); //'User with such login or e-mail does not exist'
-            }
-            // Если залогинен и пытается восстановить не свой аккаунт, то проверяем что это админ
-            if (iAm.registered && iAm.user.login !== data.login && !iAm.isAdmin) {
-                return cb({ message: msg.deny, error: true });
-            }
+    const user = await User.findOne({
+        $or: [{ login: new RegExp(`^${login}$`, 'i') }, { email: login.toLowerCase() }]
+    }, null, { lean: true }).exec();
 
-            data._id = user._id;
-            data.login = user.login;
-            data.email = user.email;
-            data.disp = user.disp;
-            confirmKey = Utils.randomString(8);
-            UserConfirm.remove({ user: user._id }, this);
-        },
-        function (err) {
-            if (err) {
-                logger.error('Auth recall UserConfirm.remove: ', err);
-                return cb(recallPublicError);
-            }
-            new UserConfirm({ key: confirmKey, user: data._id }).save(this);
-        },
-        function finish(err) {
-            if (err) {
-                logger.error('Auth recall UserConfirm.save: ', err);
-                return cb(recallPublicError);
-            }
-            cb(successPublic);
-
-            sendMail({
-                sender: 'noreply',
-                receiver: { alias: data.login, email: data.email },
-                subject: 'Запрос на восстановление пароля',
-                head: true,
-                body: recallTpl({
-                    data,
-                    config,
-                    confirmKey,
-                    username: data.disp,
-                    linkvalid: `${human2d} (до ${moment.utc().add(ms2d).format('LLL')})`
-                }),
-                text: `Перейдите по следующей ссылке: ${config.client.origin}/confirm/${confirmKey}`
-            });
-        }
-    );
-}
-
-// Смена пароля по запросу восстановлния из почты
-var passChangeRecallPublicError = { message: 'Ошибка смены пароля', error: true };
-function passChangeRecall(iAm, data, cb) {
-    var error = '',
-        key = data.key;
-
-    if (!data || !Utils.isType('string', key) || key.length !== 8) {
-        error = 'Bad params. ';
-    }
-    if (!data.pass) {
-        error += 'Fill in the password field. ';
-    }
-    if (data.pass !== data.pass2) {
-        error += 'Passwords do not match.';
-    }
-    if (error) {
-        return cb({ message: error, error: true });
+    if (!user) {
+        throw { message: 'Пользователя с таким логином или e-mail не существует' };
     }
 
-    UserConfirm.findOne({ key }).populate('user').exec(function (err, confirm) {
-        if (err) {
-            logger.error('Auth passChangeRecall UserConfirm.findOne: ', err);
-            return cb(passChangeRecallPublicError);
-        }
-        if (!confirm || !confirm.user) {
-            return cb(passChangeRecallPublicError);
-        }
-        step(
-            function () {
-                // Если залогиненный пользователь запрашивает восстановление, то пароль надо поменять в модели пользователя сессии
-                // Если аноним - то в модели пользователи конфирма
-                // (Это один и тот же пользователь, просто разные объекты)
-                var user = iAm.registered && iAm.user.login === confirm.user.login ? iAm.user : confirm.user;
-                user.pass = data.pass;
+    // If user logged in and trying t restore not own accaunt, it mast be admin
+    if (iAm.registered && iAm.user.login !== login && !iAm.isAdmin) {
+        throw { message: msg.deny };
+    }
 
-                // Если неактивный пользователь восстанавливает пароль - активируем его
-                if (!user.active) {
-                    user.active = true;
-                    user.activatedate = new Date();
-                }
+    const confirmKey = Utils.randomString(8);
+    await UserConfirm.remove({ user: user._id }).exec();
 
-                user.save(this.parallel());
-                confirm.remove(this.parallel());
-            },
-            function (err) {
-                if (err) {
-                    logger.error('Auth passChangeRecall user.save or confirm.remove: ', err);
-                    return cb(passChangeRecallPublicError);
-                }
+    await new UserConfirm({ key: confirmKey, user: user._id }).save();
 
-                cb({ message: 'Новый пароль сохранен успешно' });
-            }
-        );
+    sendMail({
+        sender: 'noreply',
+        receiver: { alias: login, email: user.email },
+        subject: 'Запрос на восстановление пароля',
+        head: true,
+        body: recallTpl({
+            config,
+            confirmKey,
+            username: user.disp,
+            linkvalid: `${human2d} (до ${moment.utc().add(ms2d).format('LLL')})`
+        }),
+        text: `Перейдите по следующей ссылке: ${config.client.origin}/confirm/${confirmKey}`
     });
+
+    return {
+        message: 'Запрос успешно отправлен. Для продолжения процедуры следуйте инструкциям, высланным на Ваш e-mail'
+    };
 }
 
-// Смена пароля в настройках пользователя с указанием текущего пароля
-var passChangePublicError = { message: 'Ошибка смены пароля', error: true };
-function passChange(iAm, data, cb) {
-    var error = '';
-
-    if (!iAm.registered || !data || iAm.user.login !== data.login) {
-        return cb({ message: 'Вы не авторизованны для этой операции', error: true }); // 'You are not authorized for this action'
+// Password hange by recall request from email
+async function passChangeRecall(iAm, { key, pass, pass2 } = {}) {
+    if (!_.isString(key) || key.length !== 8) {
+        throw { message: 'Bad params' };
     }
-    if (!data.pass || !data.passNew || !data.passNew2) {
-        error += 'Заполните все поля. '; // 'Fill in all password fields. ';
+    if (!_.isString(pass) || !pass) {
+        throw { message: 'Fill in the password field' };
     }
-    if (data.passNew !== data.passNew2) {
-        error += 'Новые пароли не совпадают. '; // 'New passwords do not match each other.';
-    }
-    if (error) {
-        return cb({ message: error, error: true });
+    if (pass !== pass2) {
+        throw { message: 'Пароли не совпадают' };
     }
 
-    iAm.user.checkPass(data.pass, function (err, isMatch) {
-        if (err) {
-            logger.error('Auth passChange iAm.user.checkPass: ', err);
-            return cb(passChangePublicError);
-        }
+    const confirm = await UserConfirm.findOne({ key }).populate('user').exec();
 
-        if (isMatch) {
-            iAm.user.pass = data.passNew;
-            iAm.user.save(function (err) {
-                if (err) {
-                    logger.error('Auth passChange iAm.user.save: ', err);
-                    return cb(passChangePublicError);
-                }
-                cb({ message: 'Новый пароль установлен успешно' }); //'Password was changed successfully!'
-            });
-        } else {
-            cb({ message: 'Текущий пароль не верен', error: true }); //'Current password incorrect'
-        }
-    });
+    if (!confirm || !confirm.user) {
+        throw { message: msg.passChangeError };
+    }
+
+    // If registered user has requested password restoration, pass must be changed in user's model in session
+    // If anonym - in user's model in confirm
+    // (it the same user, but different objects)
+    const user = iAm.registered && iAm.user.login === confirm.user.login ? iAm.user : confirm.user;
+    user.pass = pass;
+
+    // If inactive user is restoring password - activate him
+    if (!user.active) {
+        user.active = true;
+        user.activatedate = new Date();
+    }
+
+    await* [user.save(), confirm.remove()];
+
+    return { message: 'Новый пароль сохранен успешно' };
 }
 
-//Проверка ключа confirm
-var checkConfirmPublicError = { message: 'Ошибка подтверждения ключа', error: true };
-function checkConfirm(data, cb) {
-    if (!data || !Utils.isType('string', data.key) || data.key.length < 7 || data.key.length > 8) {
-        cb({ message: 'Bad params', error: true });
-        return;
+// Password changing in user's settings page with entering current password
+async function passChange(iAm, { login, pass, passNew, passNew2 } = {}) {
+    if (!iAm.registered || iAm.user.login !== login) {
+        throw { message: msg.deny };
+    }
+    if (!pass || !passNew || !passNew2) {
+        throw { message: 'Заполните все поля' };
+    }
+    if (passNew !== passNew2) {
+        throw { message: 'Пароли не совпадают' };
     }
 
-    var key = data.key;
-    UserConfirm.findOne({ key: key }).populate('user').exec(function (err, confirm) {
-        if (err) {
-            logger.error('Auth checkConfirm UserConfirm.findOne: ', err);
-            return cb(checkConfirmPublicError);
-        }
-        if (!confirm || !confirm.user) {
-            return cb({ message: 'Переданного вами ключа не существует', error: true });
-        }
-        var user = confirm.user,
-            avatar;
+    const isMatch = await iAm.user.checkPass(pass);
 
-        if (key.length === 7) { //Confirm registration
-            step(
-                function () {
-                    user.active = true;
-                    user.activatedate = new Date();
-                    user.save(this.parallel());
-                    confirm.remove(this.parallel());
-                },
-                function (err) {
-                    if (err) {
-                        logger.error('Auth checkConfirm confirm.remove: ', err);
-                        return cb(checkConfirmPublicError);
-                    }
+    if (!isMatch) {
+        throw { message: 'Текущий пароль не верен' };
+    }
 
-                    cb({
-                        message: 'Спасибо, регистрация подтверждена! Теперь вы можете войти в систему, используя ваш логин и пароль',
-                        type: 'noty'
-                    });
-                    //cb({message: 'Thank you! Your registration is confirmed. Now you can enter using your username and password', type: 'noty'});
-                }
-            );
-        } else if (key.length === 8) { //Confirm pass change
-            if (user.avatar) {
-                if (preaddrs.length) {
-                    avatar = preaddrs[0] + '/_a/h/' + user.avatar;
-                } else {
-                    avatar = '/_a/h/' + user.avatar;
-                }
-            } else {
-                avatar = '/img/caps/avatarth.png';
-            }
-            cb({ message: 'Pass change', type: 'authPassChange', login: user.login, disp: user.disp, avatar: avatar });
-        }
+    iAm.user.pass = passNew;
+    await iAm.user.save();
 
-    });
+    return { message: 'Новый пароль установлен успешно' };
 }
 
-export const ready = new Promise(async function(resolve, reject) {
-    try {
-        const [regData, recallData] = await* [
-            fs.readFileAsync(path.normalize('./views/mail/registration.jade'), 'utf-8'),
-            fs.readFileAsync(path.normalize('./views/mail/recall.jade'), 'utf-8')
-        ];
-
-        regTpl = jade.compile(regData, { filename: path.normalize('./views/mail/registration.jade'), pretty: false });
-        recallTpl = jade.compile(recallData, { filename: path.normalize('./views/mail/recall.jade'), pretty: false });
-        resolve();
-    } catch (err) {
-        err.message = 'Auth jade read error: ' + err.message;
-        reject(err);
+// Check confirm key
+async function checkConfirm({ key } = {}) {
+    if (!_.isString(key) || key.length < 7 || key.length > 8) {
+        throw { message: 'Bad params' };
     }
+
+    const confirm = await UserConfirm.findOne({ key }).populate('user').exec();
+
+    if (!confirm || !confirm.user) {
+        throw { message: 'Переданного вами ключа не существует' };
+    }
+
+    const user = confirm.user;
+
+    if (key.length === 7) { // Confirm registration
+        user.active = true;
+        user.activatedate = new Date();
+        await* [user.save(), confirm.remove()];
+
+        return {
+            message: 'Спасибо, регистрация подтверждена! Теперь вы можете войти в систему, используя ваш логин и пароль',
+            type: 'noty'
+        };
+    } else if (key.length === 8) { // Confirm password change
+        const avatar = user.avatar ? '/_a/h/' + user.avatar : '/img/caps/avatarth.png';
+
+        return { message: 'Pass change', type: 'authPassChange', login: user.login, disp: user.disp, avatar };
+    }
+}
+
+const whoAmI = iAm => Promise.resolve({
+    user: iAm.user && iAm.user.toObject ? iAm.user.toObject() : null,
+    registered: iAm.registered
 });
 
 export function loadController(io) {
@@ -446,33 +357,52 @@ export function loadController(io) {
         });
 
         socket.on('recallRequest', function (data) {
-            recall(hs.usObj, data, function (data) {
-                socket.emit('recallResult', data);
-            });
+            recall(hs.usObj, data)
+                .catch(function (err) {
+                    return { message: err.message, error: true };
+                })
+                .then(function (resultData) {
+                    socket.emit('recallResult', resultData);
+                });
         });
 
         socket.on('passChangeRecall', function (data) {
-            passChangeRecall(hs.usObj, data, function (data) {
-                socket.emit('passChangeRecallResult', data);
-            });
+            passChangeRecall(hs.usObj, data)
+                .catch(function (err) {
+                    return { message: err.message, error: true };
+                })
+                .then(function (resultData) {
+                    socket.emit('passChangeRecallResult', resultData);
+                });
         });
         socket.on('passChangeRequest', function (data) {
-            passChange(hs.usObj, data, function (data) {
-                socket.emit('passChangeResult', data);
-            });
-        });
-
-        socket.on('whoAmI', function () {
-            socket.emit('youAre', {
-                user: hs.usObj.user && hs.usObj.user.toObject ? hs.usObj.user.toObject() : null,
-                registered: hs.usObj.registered
-            });
+            passChange(hs.usObj, data)
+                .catch(function (err) {
+                    return { message: err.message, error: true };
+                })
+                .then(function (resultData) {
+                    socket.emit('passChangeResult', resultData);
+                });
         });
 
         socket.on('checkConfirm', function (data) {
-            checkConfirm(data, function (data) {
-                socket.emit('checkConfirmResult', data);
-            });
+            checkConfirm(data)
+                .catch(function (err) {
+                    return { message: err.message, error: true };
+                })
+                .then(function (resultData) {
+                    socket.emit('checkConfirmResult', resultData);
+                });
+        });
+
+        socket.on('whoAmI', function () {
+            whoAmI(hs.usObj)
+                .catch(function (err) {
+                    return { message: err.message, error: true };
+                })
+                .then(function (resultData) {
+                    socket.emit('youAre', resultData);
+                });
         });
     });
 };
