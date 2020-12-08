ea54aa946ce2c4e00dd071ac39666e6214d0e6b1
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2020-12-08 14:56:36 +0300

    Separate module for notifications (#324)
    * Clone app.js to notifier.js
    
    * Add notifier to babel, package.json and docker-compose
    
    * remove extra stuff from notifier
    
    * Reduced notifier.js even more, but still working

--- babel/server.files.js
+++ babel/server.files.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
     only: [ // May be array of regexp, or github.com/isaacs/node-glob
-        /(app|downloader|uploader|sitemap).js/,
+        /(app|downloader|uploader|sitemap|notifier).js/,
         /controllers\/((?!systemjs|api|apilog).)+\.js$/,
         'commons/time.js',
         /models\/.+\.js$/,
