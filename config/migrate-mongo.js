a1ded89a3d27e45e0cabf8f64b3ad8352b1ceb3f
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2021-05-27 22:34:28 +0100

    Replace console with logger for migrate-mongo run.
    So that output is logged.

--- config/migrate-mongo.js
+++ config/migrate-mongo.js
@@ -2,6 +2,15 @@
  * Migrate-mongo configuration.
  */
 const mongoConfig = require('./').mongo;
+const argv = require('yargs').argv;
+const logger = require('log4js').getLogger('migrate-mongo');
+
+// Replace node console with logger for up and down migration commands, so we have a
+// log record for these actions.
+if (argv._.includes('up') || argv._.includes('down')) {
+    console.log = logger.info.bind(logger);
+    console.error = logger.error.bind(logger);
+}
 
 const config = {
     mongodb: {
