e197442815543fede97fda44f3cbc24c9b402a55
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2021-10-14 15:48:05 +0300

    dev-mongo.Dockerfile

--- docker/dev-initdb.sh
+++ docker/dev-initdb.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ARCHIVE=/dump/pastvu.gz
-test -f $ARCHIVE || exit 1
-mongorestore --gzip --db pastvu --archive=$ARCHIVE
+curl -s -o /tmp/pastvu.gz https://varlamov.me/pastvu/github/pastvu.gz
+mongorestore --gzip --db pastvu --archive=/tmp/pastvu.gz
+rm /tmp/pastvu.gz
