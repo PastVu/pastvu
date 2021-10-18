1e0d987e13f43e86b9a7ff2953262899e46cd415
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2021-10-18 05:15:44 +0300

    Update default.config.js
    comment logPath

--- config/default.config.js
+++ config/default.config.js
@@ -47,7 +47,7 @@ module.exports = {
     servePublic: true, // Tell app.js serve out its public folder (js, css, etc)
     serveStore: true, // Tell app.js serve out store public folder
 
-    logPath: '', // Folder for logs. Setting that property will enable log to files in logPath (relative to project dir). By default it is off
+    // logPath: '', // Folder for logs. Setting that property will enable log to files in logPath (relative to project dir). By default it is off
     logLongDuration: ms('2s'),
     serveLog: false, // Tell app.js serve out its logs folder (path: "logPath") at /nodelog
     serveLogAuth: {
