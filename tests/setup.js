7663d384b36cd6dbbd24083759297422d54730ec
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2021-12-06 23:48:01 +0000

    tests: Minor refactoring in common setup.

--- tests/setup.js
+++ tests/setup.js
@@ -4,9 +4,45 @@ import { UserSettings } from '../models/UserSettings';
 
 jest.setTimeout(10000);
 
+// Runs before any of the tests in test file run.
 beforeAll(async () => {
     await connectDb({ mongo: { uri: process.env.MONGO_INSTANCE_URI } });
     await waitDb;
+    await seedDatabase();
+});
+
+beforeEach(async () => {
+    // This would be the place to seed database, but we only have UserSettings
+    // required so far, so no point to re-seed it before each test unless we
+    // start modifying those settings one day.
+    //await seedDatabase();
+});
+
+// Cleans up database between each test.
+afterEach(async () => {
+    const collections = mongoose.connection.collections;
+
+    for (const collectionName of Object.keys(collections)) {
+        // For now preserve UserSettings.
+        if (collectionName !== 'UserSettings') {
+            const collection = collections[collectionName];
+
+            await collection.deleteMany();
+        }
+    }
+});
+
+// Close connection when all tests in the file are completed.
+afterAll(async () => {
+    await mongoose.connection.db.dropDatabase();
+    await mongoose.connection.close();
+});
+
+async function seedDatabase() {
+    // We should probably do this a bit smarter way in future, may be define
+    // it in model and populate on init if collection is empty.
+
+    // Populate UserSettings.
     await UserSettings.insertMany([
         { key: 'subscr_auto_reply', val: true, vars: [true, false], desc: 'Автоподписка при комментировании темы' },
         { key: 'subscr_throttle', val: 3 * 60 * 60 * 1000, vars: [5 * 60 * 1000, 30 * 60 * 1000, 60 * 60 * 1000, 3 * 60 * 60 * 1000, 6 * 60 * 60 * 1000, 24 * 60 * 60 * 1000], desc: 'Минимальный интервал между отправками письма с уведомлением' },
@@ -20,24 +56,4 @@ beforeAll(async () => {
         { key: 'comment_show_deleted', val: false, vars: [true, false], desc: 'Показывать удаленные комментарии' },
         { key: 'photo_disallow_download_origin', val: false, vars: [true, false], desc: 'Disallow others users to download photo without watermark' },
     ]);
-});
-
-beforeEach(async () => {
-    //await seedDatabase()
-});
-
-// Cleans up database between each test
-afterEach(async () => {
-    const collections = mongoose.connection.collections;
-
-    for (const collectionName of Object.keys(collections)) {
-        const collection = collections[collectionName];
-
-        await collection.deleteMany();
-    }
-});
-
-afterAll(async () => {
-    await mongoose.connection.db.dropDatabase();
-    await mongoose.connection.close();
-});
+}
