e1e2862541c2ddc72f150ac8ddd113ef6998c1d4
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2021-12-08 23:46:04 +0000

    tests: Remove empty session test for now.

--- controllers/__tests__/session.test.js
+++
@@ -1,9 +0,0 @@
-import { User } from '../../models/User';
-
-test('should save user to database', async () => {
-    // Searches the user in the database
-    const user = await User.findOne({}).exec();
-
-    //console.log(user);
-    expect(user).toBeNull();
-});
