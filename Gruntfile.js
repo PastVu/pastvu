49e1c851a3ffc058b2d8545e9e680324cc698efd
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2022-10-13 20:46:02 +0100

    Add public/js/intl/*.js to linting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -254,6 +254,7 @@ module.exports = function (grunt) {
                         'public/js/model/*.js',
                         'public/js/*.js',
                         'public/js/errors/*.js',
+                        'public/js/intl/*.js',
                     ],
                 },
             },
