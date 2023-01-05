0eb5035edc61535cb0c6f4478d7c3079175957fe
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2023-01-05 10:54:32 +0000

    newsedit: Add resizeimg plugin.
    This requires depenendecy jquery-resizable (https://github.com/RickStrahl/jquery-resizable, MIT licensed)

--- public/js/module/admin/newsEdit.js
+++ public/js/module/admin/newsEdit.js
@@ -37,6 +37,8 @@ define([
         'trumbowyg-plugins/indent/trumbowyg.indent.min',
         'trumbowyg-plugins/table/trumbowyg.table.min',
         'trumbowyg-plugins/noembed/trumbowyg.noembed.min',
+        'jquery-plugins/jquery-resizable.min', // Required for trumbowyg.resizimg.min
+        'trumbowyg-plugins/resizimg/trumbowyg.resizimg.min'
     ];
 
     return Cliche.extend({
