5413912bf53e7bbcbfb2c9ed0d9b0756a2388bb5
19AnastasiaTsareva20 <19anastasia.tsareva20@gmail.com>
2025-04-30 00:53:36 +0300

    Fix UI issues with long text in profile fields

--- models/User.js
+++ models/User.js
@@ -65,10 +65,7 @@ registerModel(db => {
         firstName: { type: String },
         lastName: { type: String },
         disp: { type: String }, // Display name
-        realName: {
-            type: String,
-            maxlength: [50, 'EValidation.RealNameTooLong'],
-        },
+
         birthdate: { type: String },
         sex: { type: String },
         country: { type: String },
