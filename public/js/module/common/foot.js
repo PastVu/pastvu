13dcdedca416c3384bef476b47a13b64122bdebc
Egor Kuzmichev <egor@math.ru>
2026-06-05 11:53:47 +0800

    Translate footer and About modal
    Bind footer links (Support, Rules, About) to i18n in foot.pug and
    localize the About modal's topic and Close buttons in foot.js. Add the
    EN translations for those keys.

--- public/js/module/common/foot.js
+++ public/js/module/common/foot.js
@@ -45,13 +45,13 @@ define(['underscore', 'Params', 'knockout', 'm/_moduleCliche', 'globalVM', 'rend
                         {
                             module: 'm/diff/about',
                             modal: {
-                                topic: 'О проекте',
+                                topic: globalVM.i18n('О проекте'),
                                 initWidth: '1000px',
                                 //animateScale: true,
                                 curtainClick: { click: this.closePopup, ctx: this },
-                                offIcon: { text: 'Закрыть', click: this.closePopup, ctx: this },
+                                offIcon: { text: globalVM.i18n('Закрыть'), click: this.closePopup, ctx: this },
                                 btns: [
-                                    { css: 'btn-primary', text: 'Закрыть', click: this.closePopup, ctx: this },
+                                    { css: 'btn-primary', text: globalVM.i18n('Закрыть'), click: this.closePopup, ctx: this },
                                 ],
                             },
                             callback: function (vm) {
